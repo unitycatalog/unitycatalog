91049913517d065b10d461b5ae39db81b00c20c3
Edmondo Porcu <edmondo.porcu@gmail.com>
2024-06-14 17:19:25 -0400

    Improving SBT files

---
+++ project/Versions.scala
@@ -0,0 +1,61 @@
+object Versions {
+
+  val apacheHTTPComponents = Map(
+    
+    "httpcore" -> "4.4.16",
+    
+    "httpclient" -> "4.5.14"
+  )
+
+  val armeria = "1.28.4"
+
+  val asciiTable = "0.3.2"
+
+  val awsSDK = "1.12.728"
+
+  val byteBuddy = "1.14.15"
+
+  val commonsCLI = "1.7.0"
+
+  val delta = "3.2.0"
+
+  val findBugs = "3.0.2"
+
+  val guava = "31.0.1-jre"
+
+  val hadoop = "3.4.0"
+
+  val iceberg = "1.5.2"
+
+  val log4j = "2.23.1"
+
+  val h2 = "2.2.224"
+
+  val hibernate  ="6.5.0.Final"
+
+  val jackson = "2.17.0"
+
+  object Jakarta {
+    val activation = "2.1.3"
+    val annotation = "1.3.5"
+  }
+
+  val javaxAnnotations = "1.3.2"
+
+  val json = "20240303"
+
+  val junit = "4.13.2"
+
+  val junitJupiter = "5.9.2"
+
+  val lombok = "1.18.32"
+
+  val openApiToolsJacksonBindNullableVersion = "0.2.6"
+
+  val sbtJunitInterface = "0.13.3"
+
+  val snakeYaml = "2.2"
+
+  val vertx = "4.3.5"
+
+}
