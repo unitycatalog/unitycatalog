78645911bf375e9ac9fb2b9dd619952ef978c446
Flook Peter <pflooky@hotmail.com>
2024-06-14 08:06:23 +0800

    Add in Dockerfile, add in GitHub action to create Docker image, add build folders to gitignore

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:24-slim
+
+COPY server/target/unitycatalog-server*.jar /opt/app/unitycatalog-server.jar
+COPY examples/cli/target/unitycatalog-cli*.jar /opt/app/unitycatalog-cli.jar
+
+ENTRYPOINT ["java", "-cp", "/opt/app/unitycatalog-server.jar", "io.unitycatalog.server.UnityCatalogServer"]
---
+++ docker-action.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+APP_VERSION=$(awk -F ' := ' '{print $2}' "$ROOT_DIR/version.sbt" | tr -d '"')
+PLATFORMS="linux/amd64,linux/arm64"
+
+run_sbt() {
+  SBT_COMMAND="$ROOT_DIR/build/sbt -info clean package"
+  echo "Running SBT to generate Server and CLI JAR: $SBT_COMMAND"
+  $SBT_COMMAND || exit
+}
+
+SERVER_TARGET_DIR="$ROOT_DIR/server/target"
+SERVER_JAR=$(find "$SERVER_TARGET_DIR" -name "unitycatalog-server*.jar" | head -n 1)
+if [ -z "$SERVER_JAR" ]; then
+    echo "Server JAR not found starting with 'unitycatalog-server*' in the target directory '$SERVER_TARGET_DIR'."
+    run_sbt
+fi
+
+CLI_TARGET_DIR="$ROOT_DIR/examples/cli/target"
+CLI_JAR=$(find "$CLI_TARGET_DIR" -name "unitycatalog-cli-*.jar" | head -n 1)
+if [ -z "$CLI_JAR" ]; then
+    echo "CLI JAR not found starting with 'unitycatalog-cli-*' in the target directory '$CLI_TARGET_DIR'."
+    run_sbt
+fi
+
+docker run --privileged --rm tonistiigi/binfmt --install all
+docker buildx create --use --name builder
+docker buildx inspect --bootstrap builder
+
+docker buildx build \
+  --platform "$PLATFORMS" \
+  -t datacatering/unitycatalog:"$APP_VERSION" --push .
