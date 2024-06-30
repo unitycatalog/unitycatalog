dac29d1bde3f8dc5fcf3a5a4471652594143b6ec
小清 <62982788+597365581@users.noreply.github.com>
2024-06-30 17:25:50 +0800

    use LOGGER.error to output Exception stack for Problem localization
    use LOGGER.error to output Exception stack for Problem localization

--- server/src/main/java/io/unitycatalog/server/utils/JsonUtils.java
+++ server/src/main/java/io/unitycatalog/server/utils/JsonUtils.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import java.util.Iterator;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonUtils {
 
