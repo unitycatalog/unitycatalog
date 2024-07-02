0a70ebd7c425c82b05d23c22716b5791989738a6
Jean Boutros <jean.boutros@gmail.com>
2024-07-02 17:48:31 +0100

    Updated the build script for docker

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -3,15 +3,16 @@ set -ex;
 
 current_dir=$(pwd)
 script_dir=$(dirname $0)
+docker_dir="$script_dir/run-uc"
 
-echo "Changing directory to $script_dir"
-cd $script_dir
+echo "Changing directory to $docker_dir"
+cd $docker_dir
 
 if [[ -r Dockerfile ]]; then
     docker build -t unitycatalogue:0.1.0 .
 else
     echo "Dockerfile not found."
-    exit 1
+    exit
 fi
 
 echo "Returning to base directory $current_dir"
