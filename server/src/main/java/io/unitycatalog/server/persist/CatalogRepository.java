e019ef2a41db647a0da77aae09ee9d9de5eee04b
小清 <62982788+597365581@users.noreply.github.com>
2024-07-03 00:58:37 +0800

    use LOGGER.info to replace System.out.println for Output Log (#113)
    use LOGGER.info to replace System.out.println for Output Log
    
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [ ] If there is a related issue, make sure it is linked to this PR.
    - [ ] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    <!-- Please state what you've changed and how it might affect the users.
    -->
    
    Signed-off-by: 小清 <62982788+597365581@users.noreply.github.com>

--- server/src/main/java/io/unitycatalog/server/persist/CatalogRepository.java
+++ server/src/main/java/io/unitycatalog/server/persist/CatalogRepository.java
@@ -43,7 +43,7 @@ public class CatalogRepository {
                 }
                 session.persist(catalogInfo);
                 tx.commit();
-                System.out.println("Added catalog: " + catalogInfo.getName());
+                LOGGER.info("Added catalog: {}",catalogInfo.getName());
                 return CatalogInfoDAO.toCatalogInfo(catalogInfo);
             } catch (Exception e) {
                 tx.rollback();
