aa4e6170c755d660f2793a29e5191df6a24eaed8
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2024-07-10 18:35:25 +0200

    build/sbt javafmtAll on io.unitycatalog.server.persist and io.unitycatalog.server.service package (#166)
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [ ] If there is a related issue, make sure it is linked to this PR.
    - [ ] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    build/sbt javafmtAll on `io.unitycatalog.server.persist` and
    `io.unitycatalog.server.service` package

--- server/src/main/java/io/unitycatalog/server/persist/dao/PropertyDAO.java
+++ server/src/main/java/io/unitycatalog/server/persist/dao/PropertyDAO.java
@@ -1,19 +1,21 @@
 package io.unitycatalog.server.persist.dao;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.UuidGenerator;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
 
-//Hibernate annotations
+// Hibernate annotations
 @Entity
-@Table(name = "uc_properties", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"entity_id", "entity_type", "property_key"})})
+@Table(
+    name = "uc_properties",
+    uniqueConstraints = {
+      @UniqueConstraint(columnNames = {"entity_id", "entity_type", "property_key"})
+    })
 // Lombok annotations
 @Getter
 @Setter
@@ -23,39 +25,42 @@ import java.util.stream.Collectors;
 @ToString
 @Builder
 public class PropertyDAO {
-    @Id
-    @UuidGenerator
-    @Column(name = "id", updatable = false, nullable = false)
-    private UUID id;
+  @Id
+  @UuidGenerator
+  @Column(name = "id", updatable = false, nullable = false)
+  private UUID id;
 
-    @Column(name = "entity_id", nullable = false)
-    private UUID entityId;
+  @Column(name = "entity_id", nullable = false)
+  private UUID entityId;
 
-    @Column(name = "entity_type", nullable = false)
-    private String entityType;
+  @Column(name = "entity_type", nullable = false)
+  private String entityType;
 
-    @Column(name = "property_key", nullable = false)
-    private String key;
+  @Column(name = "property_key", nullable = false)
+  private String key;
 
-    @Column(name = "property_value", nullable = false)
-    private String value;
+  @Column(name = "property_value", nullable = false)
+  private String value;
 
-    public static List<PropertyDAO> from(Map<String, String> properties, UUID entityId, String entityType) {
-        if (properties == null) {
-            return new ArrayList<>();
-        }
-        return properties.entrySet().stream()
-                .map(entry -> PropertyDAO.builder()
-                        .key(entry.getKey())
-                        .value(entry.getValue())
-                        .entityId(entityId)
-                        .entityType(entityType)
-                        .build())
-                .collect(Collectors.toList());
+  public static List<PropertyDAO> from(
+      Map<String, String> properties, UUID entityId, String entityType) {
+    if (properties == null) {
+      return new ArrayList<>();
     }
+    return properties.entrySet().stream()
+        .map(
+            entry ->
+                PropertyDAO.builder()
+                    .key(entry.getKey())
+                    .value(entry.getValue())
+                    .entityId(entityId)
+                    .entityType(entityType)
+                    .build())
+        .collect(Collectors.toList());
+  }
 
-    public static Map<String, String> toMap(List<PropertyDAO> propertyDAOList) {
-        return propertyDAOList.stream()
-                .collect(Collectors.toMap(PropertyDAO::getKey, PropertyDAO::getValue));
-    }
+  public static Map<String, String> toMap(List<PropertyDAO> propertyDAOList) {
+    return propertyDAOList.stream()
+        .collect(Collectors.toMap(PropertyDAO::getKey, PropertyDAO::getValue));
+  }
 }
