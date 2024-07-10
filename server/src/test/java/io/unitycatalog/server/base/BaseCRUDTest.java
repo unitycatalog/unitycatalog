6e07f9e4e55ba9f8e9c50b2b10c980756fe80026
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2024-07-10 18:56:45 +0200

    build/sbt javafmtAll on tests in server module (#194)
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [ ] If there is a related issue, make sure it is linked to this PR.
    - [ ] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    This formats the tests in the`server` module

--- server/src/test/java/io/unitycatalog/server/base/BaseCRUDTest.java
+++ server/src/test/java/io/unitycatalog/server/base/BaseCRUDTest.java
@@ -1,41 +1,40 @@
 package io.unitycatalog.server.base;
 
+import static io.unitycatalog.server.utils.TestUtils.CATALOG_NAME;
+import static io.unitycatalog.server.utils.TestUtils.CATALOG_NEW_NAME;
+
 import io.unitycatalog.server.base.catalog.CatalogOperations;
+import java.util.Optional;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.util.Optional;
-
-import static io.unitycatalog.server.utils.TestUtils.CATALOG_NAME;
-import static io.unitycatalog.server.utils.TestUtils.CATALOG_NEW_NAME;
-
 @RunWith(Parameterized.class)
 public abstract class BaseCRUDTest extends BaseServerTest {
 
-    protected CatalogOperations catalogOperations;
+  protected CatalogOperations catalogOperations;
 
-    @After
-    public void cleanUp() {
-        try {
-            catalogOperations.deleteCatalog(CATALOG_NAME, Optional.of(true));
-        } catch (Exception e) {
-            // Ignore
-        }
-        try {
-            catalogOperations.deleteCatalog(CATALOG_NEW_NAME, Optional.of(true));
-        } catch (Exception e) {
-            // Ignore
-        }
+  @After
+  public void cleanUp() {
+    try {
+      catalogOperations.deleteCatalog(CATALOG_NAME, Optional.of(true));
+    } catch (Exception e) {
+      // Ignore
     }
-
-    @Before
-    @Override
-    public void setUp() {
-        super.setUp();
-        catalogOperations = createCatalogOperations(serverConfig);
+    try {
+      catalogOperations.deleteCatalog(CATALOG_NEW_NAME, Optional.of(true));
+    } catch (Exception e) {
+      // Ignore
     }
+  }
 
-    protected abstract CatalogOperations createCatalogOperations(ServerConfig serverConfig);
+  @Before
+  @Override
+  public void setUp() {
+    super.setUp();
+    catalogOperations = createCatalogOperations(serverConfig);
+  }
+
+  protected abstract CatalogOperations createCatalogOperations(ServerConfig serverConfig);
 }
