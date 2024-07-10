d0f4651a49117ae1aefc5dde5ea31467daff8762
Shawn Chen <shawn.chen@databricks.com>
2024-07-10 15:22:29 -0700

    [ui] introduce api hooks with react-query (unitycatalog/unitycatalog-ui#4)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import CatalogDetails from './components/CatalogDetails';
 import SchemaDetails from './components/SchemaDetails';
@@ -8,12 +9,13 @@ import TableDetails from './components/TableDetails';
 import VolumeDetails from './components/VolumeDetails';
 import FunctionDetails from './components/FunctionDetails';
 import SchemaBrowser from './components/SchemaBrowser';
+import CatalogsList from './components/CatalogsList';
 
 const router = createBrowserRouter([
   {
     path: '/',
     Component() {
-      return <>home</>;
+      return <CatalogsList />;
     },
   },
   {
@@ -49,44 +51,51 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <Layout>
-      {/* Header */}
-      <Layout.Header style={{ display: 'flex', alignItems: 'center' }}>
-        <div></div>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['catalogs']}
-          items={[{ key: 'catalogs', label: 'Catalogs' }]}
-          style={{ flex: 1, minWidth: 0 }}
-        />
-      </Layout.Header>
-      {/* Content */}
-      <Layout.Content
-        style={{
-          height: 'calc(100vh - 64px)',
-          backgroundColor: '#fff',
-          display: 'flex',
-        }}
-      >
-        {/* Left: Schema Browser */}
-        <div
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        {/* Header */}
+        <Layout.Header style={{ display: 'flex', alignItems: 'center' }}>
+          <div></div>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['catalogs']}
+            items={[{ key: 'catalogs', label: 'Catalogs' }]}
+            style={{ flex: 1, minWidth: 0 }}
+          />
+        </Layout.Header>
+        {/* Content */}
+        <Layout.Content
           style={{
-            width: '30%',
-            minWidth: 260,
-            maxWidth: 400,
-            borderRight: '1px solid lightgrey',
+            height: 'calc(100vh - 64px)',
+            backgroundColor: '#fff',
+            display: 'flex',
           }}
         >
-          <SchemaBrowser />
-        </div>
-        {/* Right: Main details content */}
-        <div style={{ overflowY: 'scroll', flex: 1, padding: 16 }}>
-          <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
-        </div>
-      </Layout.Content>
-    </Layout>
+          {/* Left: Schema Browser */}
+          <div
+            style={{
+              width: '30%',
+              minWidth: 260,
+              maxWidth: 400,
+              borderRight: '1px solid lightgrey',
+            }}
+          >
+            <SchemaBrowser />
+          </div>
+
+          {/* Right: Main details content */}
+          <div style={{ overflowY: 'scroll', flex: 1, padding: 16 }}>
+            <RouterProvider
+              router={router}
+              fallbackElement={<p>Loading...</p>}
+            />
+          </div>
+        </Layout.Content>
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
