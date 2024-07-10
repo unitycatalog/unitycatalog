540da0adac2db698ec69f61fe722bb4dadec2d7b
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2024-07-10 18:49:42 +0200

    build/sbt javafmtAll on multiple packages in server module (#167)
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [ ] If there is a related issue, make sure it is linked to this PR.
    - [ ] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    This formats multiple packages in the `server` module

--- server/src/main/java/io/unitycatalog/server/UnityCatalogServer.java
+++ server/src/main/java/io/unitycatalog/server/UnityCatalogServer.java
@@ -42,9 +42,9 @@ public class UnityCatalogServer {
 
   private void addServices(ServerBuilder sb) {
     ObjectMapper unityMapper =
-      JsonMapper.builder().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).build();
+        JsonMapper.builder().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).build();
     JacksonRequestConverterFunction unityConverterFunction =
-      new JacksonRequestConverterFunction(unityMapper);
+        new JacksonRequestConverterFunction(unityMapper);
 
     // Add support for Unity Catalog APIs
     CatalogService catalogService = new CatalogService();
@@ -52,26 +52,32 @@ public class UnityCatalogServer {
     VolumeService volumeService = new VolumeService();
     TableService tableService = new TableService();
     FunctionService functionService = new FunctionService();
-    TemporaryTableCredentialsService temporaryTableCredentialsService = new TemporaryTableCredentialsService();
-    TemporaryVolumeCredentialsService temporaryVolumeCredentialsService = new TemporaryVolumeCredentialsService();
+    TemporaryTableCredentialsService temporaryTableCredentialsService =
+        new TemporaryTableCredentialsService();
+    TemporaryVolumeCredentialsService temporaryVolumeCredentialsService =
+        new TemporaryVolumeCredentialsService();
     sb.service("/", (ctx, req) -> HttpResponse.of("Hello, Unity Catalog!"))
-      .annotatedService(basePath + "catalogs", catalogService, unityConverterFunction)
-      .annotatedService(basePath + "schemas", schemaService, unityConverterFunction)
-      .annotatedService(basePath + "volumes", volumeService, unityConverterFunction)
-      .annotatedService(basePath, tableService, unityConverterFunction)
-      .annotatedService(basePath + "functions", functionService, unityConverterFunction)
-      .annotatedService(basePath + "temporary-table-credentials", temporaryTableCredentialsService)
-      .annotatedService(basePath + "temporary-volume-credentials", temporaryVolumeCredentialsService);
+        .annotatedService(basePath + "catalogs", catalogService, unityConverterFunction)
+        .annotatedService(basePath + "schemas", schemaService, unityConverterFunction)
+        .annotatedService(basePath + "volumes", volumeService, unityConverterFunction)
+        .annotatedService(basePath, tableService, unityConverterFunction)
+        .annotatedService(basePath + "functions", functionService, unityConverterFunction)
+        .annotatedService(
+            basePath + "temporary-table-credentials", temporaryTableCredentialsService)
+        .annotatedService(
+            basePath + "temporary-volume-credentials", temporaryVolumeCredentialsService);
 
     // Add support for Iceberg REST APIs
     ObjectMapper icebergMapper = RESTObjectMapper.mapper();
     JacksonRequestConverterFunction icebergRequestConverter =
-      new JacksonRequestConverterFunction(icebergMapper);
+        new JacksonRequestConverterFunction(icebergMapper);
     JacksonResponseConverterFunction icebergResponseConverter =
-      new JacksonResponseConverterFunction(icebergMapper);
-    sb.annotatedService(basePath + "iceberg",
-      new IcebergRestCatalogService(catalogService, schemaService, tableService),
-      icebergRequestConverter, icebergResponseConverter);
+        new JacksonResponseConverterFunction(icebergMapper);
+    sb.annotatedService(
+        basePath + "iceberg",
+        new IcebergRestCatalogService(catalogService, schemaService, tableService),
+        icebergRequestConverter,
+        icebergResponseConverter);
   }
 
   public static void main(String[] args) {
@@ -101,16 +107,18 @@ public class UnityCatalogServer {
 
   private void printArt() {
     String art =
-      "################################################################### \n" +
-        "#  _    _       _ _            _____      _        _              #\n" +
-        "# | |  | |     (_) |          / ____|    | |      | |             #\n" +
-        "# | |  | |_ __  _| |_ _   _  | |     __ _| |_ __ _| | ___   __ _  #\n" +
-        "# | |  | | '_ \\| | __| | | | | |    / _` | __/ _` | |/ _ \\ / _` | #\n" +
-        "# | |__| | | | | | |_| |_| | | |___| (_| | || (_| | | (_) | (_| | #\n" +
-        "#  \\____/|_| |_|_|\\__|\\__, |  \\_____\\__,_|\\__\\__,_|_|\\___/ \\__, | #\n" +
-        "#                      __/ |                                __/ | #\n" +
-        "#                     |___/               " + String.format("%15s", ("v" + VersionUtils.VERSION)) + "  |___/  #\n" +
-        "###################################################################\n";
+        "################################################################### \n"
+            + "#  _    _       _ _            _____      _        _              #\n"
+            + "# | |  | |     (_) |          / ____|    | |      | |             #\n"
+            + "# | |  | |_ __  _| |_ _   _  | |     __ _| |_ __ _| | ___   __ _  #\n"
+            + "# | |  | | '_ \\| | __| | | | | |    / _` | __/ _` | |/ _ \\ / _` | #\n"
+            + "# | |__| | | | | | |_| |_| | | |___| (_| | || (_| | | (_) | (_| | #\n"
+            + "#  \\____/|_| |_|_|\\__|\\__, |  \\_____\\__,_|\\__\\__,_|_|\\___/ \\__, | #\n"
+            + "#                      __/ |                                __/ | #\n"
+            + "#                     |___/               "
+            + String.format("%15s", ("v" + VersionUtils.VERSION))
+            + "  |___/  #\n"
+            + "###################################################################\n";
     System.out.println(art);
   }
 }
--- server/src/main/java/io/unitycatalog/server/utils/ColumnUtils.java
+++ server/src/main/java/io/unitycatalog/server/utils/ColumnUtils.java
@@ -1,60 +1,68 @@
 package io.unitycatalog.server.utils;
 
-import io.unitycatalog.server.model.ColumnInfo;
 import io.unitycatalog.server.model.ColumnTypeName;
 import io.unitycatalog.server.persist.dao.ColumnInfoDAO;
-
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
 public class ColumnUtils {
 
-    static Map<String, String> typeNameVsTypeText = new HashMap<String, String>() {{
-        put("LONG", "bigint");
-        put("SHORT", "smallint");
-        put("BYTE", "tinyint");
-    }};
-
-    static  Map<String, String> typeNameVsJsonText = new HashMap<String, String>() {{
-        put("INT", "integer");
-    }};
-
-    static String getTypeText(ColumnTypeName typeName) {
-        return typeNameVsTypeText.getOrDefault(typeName.getValue(), typeName.getValue().toLowerCase(Locale.ROOT));
-    }
-
-    static String getTypeJson(ColumnTypeName typeName, String columnName, boolean isNullable, String precisionAndScale) {
-
-        return String
-                .format("{\"name\":\"%s\",\"type\":\"%s\",\"nullable\":%s,\"metadata\":{}}",
-                        columnName,
-                        typeNameVsJsonText.getOrDefault(typeName.getValue(), typeName.getValue().toLowerCase(Locale.ROOT))
-                                + (precisionAndScale != null ? precisionAndScale : ""),
-                        isNullable);
-    }
-
-    static String getPrecisionAndScale(ColumnInfoDAO columnInfoDAO) {
-        if (columnInfoDAO.getTypePrecision() == null || columnInfoDAO.getTypeScale() == null) {
-            columnInfoDAO.setTypePrecision(0);
-            columnInfoDAO.setTypeScale(0);
-            return "";
+  static Map<String, String> typeNameVsTypeText =
+      new HashMap<String, String>() {
+        {
+          put("LONG", "bigint");
+          put("SHORT", "smallint");
+          put("BYTE", "tinyint");
         }
-        return getPrecisionAndScale(columnInfoDAO.getTypePrecision(), columnInfoDAO.getTypeScale()) ;
-    }
+      };
 
-    public  static String getPrecisionAndScale(int precision, int scale) {
-        return "(" + precision + "," + scale + ")";
-    }
+  static Map<String, String> typeNameVsJsonText =
+      new HashMap<String, String>() {
+        {
+          put("INT", "integer");
+        }
+      };
 
-    public static void addTypeTextAndJsonText(ColumnInfoDAO columnInfoDAO) {
-        String typeName = columnInfoDAO.getTypeName();
-        columnInfoDAO.setTypeText(typeNameVsTypeText.getOrDefault(typeName, typeName.toLowerCase(Locale.ROOT)));
-        columnInfoDAO.setTypeJson(getTypeJson(
-                ColumnTypeName.valueOf(columnInfoDAO.getTypeName()),
-                columnInfoDAO.getName(),
-                columnInfoDAO.isNullable(),
-                getPrecisionAndScale(columnInfoDAO)));
-    }
+  static String getTypeText(ColumnTypeName typeName) {
+    return typeNameVsTypeText.getOrDefault(
+        typeName.getValue(), typeName.getValue().toLowerCase(Locale.ROOT));
+  }
 
+  static String getTypeJson(
+      ColumnTypeName typeName, String columnName, boolean isNullable, String precisionAndScale) {
+
+    return String.format(
+        "{\"name\":\"%s\",\"type\":\"%s\",\"nullable\":%s,\"metadata\":{}}",
+        columnName,
+        typeNameVsJsonText.getOrDefault(
+                typeName.getValue(), typeName.getValue().toLowerCase(Locale.ROOT))
+            + (precisionAndScale != null ? precisionAndScale : ""),
+        isNullable);
+  }
+
+  static String getPrecisionAndScale(ColumnInfoDAO columnInfoDAO) {
+    if (columnInfoDAO.getTypePrecision() == null || columnInfoDAO.getTypeScale() == null) {
+      columnInfoDAO.setTypePrecision(0);
+      columnInfoDAO.setTypeScale(0);
+      return "";
+    }
+    return getPrecisionAndScale(columnInfoDAO.getTypePrecision(), columnInfoDAO.getTypeScale());
+  }
+
+  public static String getPrecisionAndScale(int precision, int scale) {
+    return "(" + precision + "," + scale + ")";
+  }
+
+  public static void addTypeTextAndJsonText(ColumnInfoDAO columnInfoDAO) {
+    String typeName = columnInfoDAO.getTypeName();
+    columnInfoDAO.setTypeText(
+        typeNameVsTypeText.getOrDefault(typeName, typeName.toLowerCase(Locale.ROOT)));
+    columnInfoDAO.setTypeJson(
+        getTypeJson(
+            ColumnTypeName.valueOf(columnInfoDAO.getTypeName()),
+            columnInfoDAO.getName(),
+            columnInfoDAO.isNullable(),
+            getPrecisionAndScale(columnInfoDAO)));
+  }
 }
