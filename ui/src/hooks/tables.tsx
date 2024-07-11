e68fae1654e4936f6ba31c19aefa6e60d825e20f
Robert Martinez <robert.martinez@databricks.com>
2024-07-11 16:06:56 -0400

    [ui] feat: ✨ Add TableDetails page + Table Sidebar details + fix metadata labels (unitycatalog/unitycatalog-ui#13)
    Signed-off-by: Kevin Wang <kevin@eventualcomputing.com>

--- ui/src/hooks/tables.tsx
+++ ui/src/hooks/tables.tsx
@@ -1,14 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { UC_API_PREFIX } from '../utils/constants';
 
+interface ColumnInterface {
+  name: string;
+  type_text: string;
+  type_name: string;
+  created_at: number;
+}
+
 export interface TableInterface {
   table_id: string;
+  table_type: string;
   catalog_name: string;
   schema_name: string;
   name: string;
   comment: string;
   created_at: number;
   updated_at: number | null;
+  data_source_format: string;
+  columns: ColumnInterface[];
 }
 interface ListTablesResponse {
   tables: TableInterface[];
@@ -32,6 +42,24 @@ export function useListTables({ catalog, schema }: ListTablesParams) {
       const response = await fetch(
         `${UC_API_PREFIX}/tables?${searchParams.toString()}`
       );
+      return response.json();
+    },
+  });
+}
+
+interface GetTableParams {
+  catalog: string;
+  schema: string;
+  table: string;
+}
+
+export function useGetTable({ catalog, schema, table }: GetTableParams) {
+  return useQuery<TableInterface>({
+    queryKey: ['getTable', catalog, schema, table],
+    queryFn: async () => {
+      const fullName = [catalog, schema, table].join('.');
+
+      const response = await fetch(`${UC_API_PREFIX}/tables/${fullName}`);
       return response.json();
     },
   });
