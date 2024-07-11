85e8a323eb86423fcde6a0e2f129be86c069212b
Robert Martinez <robert.martinez@databricks.com>
2024-07-11 12:31:17 -0400

    feat: :sparkles: Add CatalogsList page + ListWrapper (#5)
    * feat: :sparkles: Add CatalogsList page + ListWrapper
    
    * extract format timestamp to util + add interaction to list rows

---
+++ src/utils/formatTimestamp.ts
@@ -0,0 +1,10 @@
+export function formatTimestamp(timestamp: number): string {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  }).format(new Date(timestamp));
+}
