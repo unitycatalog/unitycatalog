7324a524ea41608a335688e581bd06ad7bb2bd80
Shawn Chen <shawn.chen@databricks.com>
2024-07-11 11:05:57 -0700

    [ui] add basic schema browser (unitycatalog/unitycatalog-ui#8)

--- ui/src/components/SchemaBrowser.tsx
+++ ui/src/components/SchemaBrowser.tsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useListCatalogs } from '../hooks/catalog';
+import { Tree, TreeDataNode, Typography } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 
 export default function SchemaBrowser() {
-  return <div>SchemaBrowser</div>;
+  const listCatalogsRequest = useListCatalogs();
+  const [treeData, setTreeData] = useState<TreeDataNode[]>([]);
+
+  useEffect(() => {
+    setTreeData(
+      listCatalogsRequest.data?.catalogs.map((catalog) => {
+        const catalogNode: TreeDataNode = {
+          title: catalog.name,
+          key: catalog.id,
+          children: [
+            {
+              title: 'TODO',
+              key: `${catalog.id}-no-data`,
+              isLeaf: true,
+            },
+          ],
+        };
+        return catalogNode;
+      }) ?? []
+    );
+  }, [listCatalogsRequest.data?.catalogs]);
+
+  return (
+    <div>
+      <Typography.Title
+        level={4}
+        style={{
+          padding: '8px 12px',
+        }}
+      >
+        Browse
+      </Typography.Title>
+
+      <div style={{ margin: '0 12px' }}>
+        <Tree showLine switcherIcon={<DownOutlined />} treeData={treeData} />
+      </div>
+    </div>
+  );
 }
