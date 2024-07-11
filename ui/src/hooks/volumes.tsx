58174db54cc5199b635bd8994471c0cbc0b4e9ec
Robert Martinez <robert.martinez@databricks.com>
2024-07-11 15:45:57 -0400

    [ui] feat: ✨ Add schema tabs for functions tables and volumes (#12)
    * feat: :sparkles: Add schema tabs for functions tables and volumes

---
+++ ui/src/hooks/volumes.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+import { UC_API_PREFIX } from '../utils/constants';
+
+export interface VolumeInterface {
+  table_id: string;
+  catalog_name: string;
+  schema_name: string;
+  name: string;
+  comment: string;
+  created_at: number;
+  updated_at: number | null;
+}
+interface ListVolumesResponse {
+  volumes: VolumeInterface[];
+  next_page_token: string | null;
+}
+
+interface ListVolumesParams {
+  catalog: string;
+  schema: string;
+}
+
+export function useListVolumes({ catalog, schema }: ListVolumesParams) {
+  return useQuery<ListVolumesResponse>({
+    queryKey: ['listVolumes', catalog, schema],
+    queryFn: async () => {
+      const searchParams = new URLSearchParams({
+        catalog_name: catalog,
+        schema_name: schema,
+      });
+
+      const response = await fetch(
+        `${UC_API_PREFIX}/volumes?${searchParams.toString()}`
+      );
+      return response.json();
+    },
+  });
+}
