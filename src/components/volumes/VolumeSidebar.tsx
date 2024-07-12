c9a8977694c90800f07024663303b58b0943744c
Robert Martinez <robert.martinez@databricks.com>
2024-07-12 13:47:05 -0400

    feat: :sparkles: Add VolumeDetailsPage + Volume sidebar (#14)
    * feat: ✨ Add TableDetails page + Table Sidebar details + fix metadata labels
    
    * add volume icon to details header

---
+++ src/components/volumes/VolumeSidebar.tsx
@@ -0,0 +1,53 @@
+import { Typography } from 'antd';
+import { formatTimestamp } from '../../utils/formatTimestamp';
+import MetadataList, { MetadataListType } from '../MetadataList';
+import { useGetVolume, VolumeInterface } from '../../hooks/volumes';
+
+interface VolumeSidebarProps {
+  catalog: string;
+  schema: string;
+  volume: string;
+}
+
+const VOLUME_METADATA: MetadataListType<VolumeInterface> = [
+  {
+    key: 'created_at',
+    dataIndex: 'created_at',
+    label: 'Created at',
+    render: (value) => (
+      <Typography.Text>{formatTimestamp(value)}</Typography.Text>
+    ),
+  },
+  {
+    key: 'updated_at',
+    dataIndex: 'updated_at',
+    label: 'Updated at',
+    render: (value) => (
+      <Typography.Text>{formatTimestamp(value)}</Typography.Text>
+    ),
+  },
+  {
+    key: 'volume_type',
+    label: 'Volume type',
+    dataIndex: 'volume_type',
+    render: (value) => <Typography.Text code>{value}</Typography.Text>,
+  },
+];
+
+export default function VolumeSidebar({
+  catalog,
+  schema,
+  volume,
+}: VolumeSidebarProps) {
+  const { data } = useGetVolume({ catalog, schema, volume });
+
+  if (!data) return null;
+
+  return (
+    <MetadataList
+      data={data}
+      metadata={VOLUME_METADATA}
+      title="Volume details"
+    />
+  );
+}
