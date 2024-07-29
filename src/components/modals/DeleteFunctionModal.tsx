d8d0acbc0c493d3525e892f77af1c62c59027c01
Xiang Xu <xiang.xu@databricks.com>
2024-07-29 10:57:19 -0700

    Add 'delete function' similar to delete table (#52)

---
+++ src/components/modals/DeleteFunctionModal.tsx
@@ -0,0 +1,67 @@
+import { Modal, Typography } from 'antd';
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDeleteFunction } from '../../hooks/functions';
+import { useNotification } from '../../utils/NotificationContext';
+
+interface DeleteFunctionModalProps {
+  open: boolean;
+  closeModal: () => void;
+  catalog: string;
+  schema: string;
+  ucFunction: string;
+}
+
+export function DeleteFunctionModal({
+  open,
+  closeModal,
+  catalog,
+  schema,
+  ucFunction,
+}: DeleteFunctionModalProps) {
+  const navigate = useNavigate();
+  const { setNotification } = useNotification();
+  const mutation = useDeleteFunction({
+    onSuccessCallback: () => {
+      setNotification(`${ucFunction} function successfully deleted`, 'success');
+      navigate(`/data/${catalog}/${schema}`);
+    },
+    catalog,
+    schema,
+  });
+
+  const handleSubmit = useCallback(() => {
+    mutation.mutate(
+      { catalog_name: catalog, schema_name: schema, name: ucFunction },
+      {
+        onError: (error: Error) => {
+          setNotification(error.message, 'error');
+        },
+      },
+    );
+  }, [mutation, ucFunction, setNotification]);
+
+  return (
+    <Modal
+      title={
+        <Typography.Title type={'danger'} level={4}>
+          Delete function
+        </Typography.Title>
+      }
+      okText="Delete"
+      okType="danger"
+      cancelText="Cancel"
+      open={open}
+      destroyOnClose
+      onCancel={closeModal}
+      onOk={handleSubmit}
+      okButtonProps={{ loading: mutation.isPending }}
+    >
+      <Typography.Text>
+        Are you sure you want to delete the function
+      </Typography.Text>
+      <Typography.Text strong>{` ${ucFunction}`}</Typography.Text>
+      <Typography.Text>? This operation cannot be undone.</Typography.Text>
+    </Modal>
+  );
+}
