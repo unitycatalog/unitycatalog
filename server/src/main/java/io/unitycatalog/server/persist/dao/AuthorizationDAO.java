adb99e832d1ef6a51a2097de496628e6b9d61612
Vikrant Puppala <vikrant.puppala@databricks.com>
2024-07-31 16:20:16 +0530

    create DAOs

---
+++ server/src/main/java/io/unitycatalog/server/persist/dao/AuthorizationDAO.java
@@ -0,0 +1,45 @@
+package io.unitycatalog.server.persist.dao;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "uc_authorizations")
+// Lombok annotations
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@SuperBuilder
+public class AuthorizationDAO {
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "principal_id", nullable = false)
+    private UUID principalId;
+
+    @Column(name = "principal_type")
+    private String principalType;
+
+    @Column(name = "privilege")
+    private String privilege;
+
+    @Column(name = "resource_id", nullable = false)
+    private UUID resourceId;
+
+    @Column(name = "resource_type")
+    private String resourceType;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+}
---
+++ server/src/main/java/io/unitycatalog/server/persist/dao/UserDAO.java
@@ -0,0 +1,36 @@
+package io.unitycatalog.server.persist.dao;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "uc_users")
+// Lombok annotations
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+public class UserDAO extends IdentifiableDAO {
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "external_id")
+    private String externalId;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
