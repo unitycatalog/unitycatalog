f3e55c03447f0558b5380f3c838aec526d4753ab
Kris Concepcion <kris.concepcion@databricks.com>
2024-08-01 09:54:51 -0400

    Format changes

--- server/src/main/java/io/unitycatalog/server/persist/dao/RegisteredModelInfoDAO.java
+++ server/src/main/java/io/unitycatalog/server/persist/dao/RegisteredModelInfoDAO.java
@@ -1,12 +1,9 @@
 package io.unitycatalog.server.persist.dao;
 
-import io.unitycatalog.server.model.DataSourceFormat;
 import io.unitycatalog.server.model.RegisteredModelInfo;
-import io.unitycatalog.server.model.TableType;
 import io.unitycatalog.server.persist.utils.FileUtils;
 import jakarta.persistence.*;
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -14,10 +11,10 @@ import lombok.experimental.SuperBuilder;
 // Hibernate annotations
 @Entity
 @Table(
-        name = "uc_registered_models",
-        indexes = {
-                @Index(name = "idx_name", columnList = "name"),
-        })
+    name = "uc_registered_models",
+    indexes = {
+      @Index(name = "idx_name", columnList = "name"),
+    })
 // Lombok annotations
 @Getter
 @Setter
@@ -26,50 +23,55 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(callSuper = true)
 @SuperBuilder
 public class RegisteredModelInfoDAO extends IdentifiableDAO {
-    @Column(name = "schema_id")
-    private UUID schemaId;
+  @Column(name = "schema_id")
+  private UUID schemaId;
 
-    @Column(name = "created_at")
-    private Date createdAt;
+  @Column(name = "created_at")
+  private Date createdAt;
 
-    @Column(name = "created_by")
-    private String createdBy;
+  @Column(name = "created_by")
+  private String createdBy;
 
-    @Column(name = "updated_at")
-    private Date updatedAt;
+  @Column(name = "updated_at")
+  private Date updatedAt;
 
-    @Column(name = "updated_by")
-    private String updatedBy;
+  @Column(name = "updated_by")
+  private String updatedBy;
 
-    @Column(name = "comment", length = 65535)
-    private String comment;
+  @Column(name = "comment", length = 65535)
+  private String comment;
 
-    @Column(name = "url", length = 2048)
-    private String url;
+  @Column(name = "url", length = 2048)
+  private String url;
 
-    public static RegisteredModelInfoDAO from(RegisteredModelInfo registeredModelInfo) {
-        return RegisteredModelInfoDAO.builder()
-                .id(UUID.fromString(registeredModelInfo.getModelId()))
-                .name(registeredModelInfo.getName())
-                .comment(registeredModelInfo.getComment())
-                .createdAt(
-                        registeredModelInfo.getCreatedAt() != null ? new Date(registeredModelInfo.getCreatedAt()) : new Date())
-                .createdBy(registeredModelInfo.getCretedBy())
-                .updatedAt(registeredModelInfo.getUpdatedAt() != null ? new Date(registeredModelInfo.getUpdatedAt()) : new Date())
-                .createdBy(registeredModelInfo.getUpdatedBy())
-                .url(registeredModelInfo.getStorageLocation())
-                .build();
-    }
+  public static RegisteredModelInfoDAO from(RegisteredModelInfo registeredModelInfo) {
+    return RegisteredModelInfoDAO.builder()
+        .id(UUID.fromString(registeredModelInfo.getModelId()))
+        .name(registeredModelInfo.getName())
+        .comment(registeredModelInfo.getComment())
+        .createdAt(
+            registeredModelInfo.getCreatedAt() != null
+                ? new Date(registeredModelInfo.getCreatedAt())
+                : new Date())
+        .createdBy(registeredModelInfo.getCreatedBy())
+        .updatedAt(
+            registeredModelInfo.getUpdatedAt() != null
+                ? new Date(registeredModelInfo.getUpdatedAt())
+                : new Date())
+        .createdBy(registeredModelInfo.getUpdatedBy())
+        .url(registeredModelInfo.getStorageLocation())
+        .build();
+  }
 
-    public RegisteredModelInfo toRegisteredModelInfo() {
-        RegisteredModelInfo registeredModelInfo =
-                new RegisteredModelInfo()
-                        .modelId(getId().toString())
-                        .name(getName())
-                        .storageLocation(FileUtils.convertRelativePathToURI(url))
-                        .comment(comment)
-                        .createdAt(createdAt != null ? createdAt.getTime() : null)
-                        .updatedAt(updatedAt != null ? updatedAt.getTime() : null);
-        return registeredModelInfo;
-    }
+  public RegisteredModelInfo toRegisteredModelInfo() {
+    RegisteredModelInfo registeredModelInfo =
+        new RegisteredModelInfo()
+            .modelId(getId().toString())
+            .name(getName())
+            .storageLocation(FileUtils.convertRelativePathToURI(url))
+            .comment(comment)
+            .createdAt(createdAt != null ? createdAt.getTime() : null)
+            .updatedAt(updatedAt != null ? updatedAt.getTime() : null);
+    return registeredModelInfo;
+  }
 }
--- server/src/main/java/io/unitycatalog/server/service/ModelService.java
+++ server/src/main/java/io/unitycatalog/server/service/ModelService.java
@@ -12,41 +12,38 @@ import java.util.Optional;
 @ExceptionHandler(GlobalExceptionHandler.class)
 public class ModelService {
 
-    private static final ModelRepository MODEL_REPOSITORY = ModelRepository.getInstance();
+  private static final ModelRepository MODEL_REPOSITORY = ModelRepository.getInstance();
 
-    public ModelService() {}
+  public ModelService() {}
 
-    @Post("")
-    public HttpResponse createRegisteredModel(CreateRegisteredModel createRegisteredModel) {
-        assert createRegisteredModel != null;
-        RegisteredModelInfo createRegisteredModelResponse = MODEL_REPOSITORY.createRegisteredModel(createRegisteredModel);
-        return HttpResponse.ofJson(createRegisteredModelResponse);
-    }
+  @Post("")
+  public HttpResponse createRegisteredModel(CreateRegisteredModel createRegisteredModel) {
+    assert createRegisteredModel != null;
+    RegisteredModelInfo createRegisteredModelResponse =
+        MODEL_REPOSITORY.createRegisteredModel(createRegisteredModel);
+    return HttpResponse.ofJson(createRegisteredModelResponse);
+  }
 
-    @Get("/{full_name_arg}")
-    public HttpResponse getRegisteredModel(@Param("full_name_arg") String fullNameArg) {
-        assert fullNameArg != null;
-        RegisteredModelInfo registeredModelInfo = MODEL_REPOSITORY.getRegisteredModel(fullNameArg);
-        return HttpResponse.ofJson(registeredModelInfo);
-    }
+  @Get("/{full_name_arg}")
+  public HttpResponse getRegisteredModel(@Param("full_name_arg") String fullNameArg) {
+    assert fullNameArg != null;
+    RegisteredModelInfo registeredModelInfo = MODEL_REPOSITORY.getRegisteredModel(fullNameArg);
+    return HttpResponse.ofJson(registeredModelInfo);
+  }
 
-    @Get("")
-    public HttpResponse listRegisteredModels(
-            @Param("catalog_name") String catalogName,
-            @Param("schema_name") String schemaName,
-            @Param("max_results") Optional<Integer> maxResults,
-            @Param("page_token") Optional<String> pageToken) {
-        return HttpResponse.ofJson(
-                MODEL_REPOSITORY.listRegisteredModels(
-                        catalogName,
-                        schemaName,
-                        maxResults,
-                        pageToken));
-    }
+  @Get("")
+  public HttpResponse listRegisteredModels(
+      @Param("catalog_name") String catalogName,
+      @Param("schema_name") String schemaName,
+      @Param("max_results") Optional<Integer> maxResults,
+      @Param("page_token") Optional<String> pageToken) {
+    return HttpResponse.ofJson(
+        MODEL_REPOSITORY.listRegisteredModels(catalogName, schemaName, maxResults, pageToken));
+  }
 
-    @Delete("/{full_name_arg}")
-    public HttpResponse deleteRegisteredModel(@Param("full_name_arg") String fullNameArg) {
-        MODEL_REPOSITORY.deleteRegisteredModel(fullNameArg);
-        return HttpResponse.of(HttpStatus.OK);
-    }
+  @Delete("/{full_name_arg}")
+  public HttpResponse deleteRegisteredModel(@Param("full_name_arg") String fullNameArg) {
+    MODEL_REPOSITORY.deleteRegisteredModel(fullNameArg);
+    return HttpResponse.of(HttpStatus.OK);
+  }
 }
