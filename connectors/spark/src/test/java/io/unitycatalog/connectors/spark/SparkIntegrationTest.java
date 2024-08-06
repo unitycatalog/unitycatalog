ff76fe53aa6db79160ede48cf125ec9f70e4b4c5
Rui Wang <rui.wang@databricks.com>
2024-08-06 14:09:45 -0700

    update

--- connectors/spark/src/test/java/io/unitycatalog/connectors/spark/SparkIntegrationTest.java
+++ connectors/spark/src/test/java/io/unitycatalog/connectors/spark/SparkIntegrationTest.java
@@ -19,8 +19,6 @@ import io.unitycatalog.server.sdk.tables.SdkTableOperations;
 import io.unitycatalog.server.utils.TestUtils;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
 import org.apache.spark.network.util.JavaUtils;
 import org.apache.spark.sql.AnalysisException;
@@ -312,15 +310,11 @@ public class SparkIntegrationTest extends BaseCRUDTest {
   public void testCreateExternalParquetTable() throws ApiException, IOException {
     createCommonResources();
     SparkSession session = createSparkSessionWithCatalogs(CATALOG_NAME);
-    Path tempDirectory = Files.createTempDirectory("testCreateExternalTable");
+    String path = generateTableLocation(CATALOG_NAME, PARQUET_TABLE);
     String fullTableName = CATALOG_NAME + "." + SCHEMA_NAME + "." + PARQUET_TABLE;
     session
         .sql(
-            "CREATE TABLE "
-                + fullTableName
-                + "(name STRING) USING PARQUET LOCATION '"
-                + tempDirectory.toString()
-                + "'")
+            "CREATE TABLE " + fullTableName + "(name STRING) USING PARQUET LOCATION '" + path + "'")
         .collect();
     assertTrue(session.catalog().tableExists(fullTableName));
     TableInfo tableInfo = tableOperations.getTable(fullTableName);
