c9589f7a2938e58910df03c111d06a7dafbfaa0d
Joseph Mocker <mock@fakebelieve.org>
2024-08-16 08:54:03 -0700

    Adding authorizations to service endpoints.

--- server/src/main/java/io/unitycatalog/server/service/CatalogService.java
+++ server/src/main/java/io/unitycatalog/server/service/CatalogService.java
@@ -1,46 +1,126 @@
 package io.unitycatalog.server.service;
 
+import static io.unitycatalog.server.model.ResourceType.CATALOG;
+import static io.unitycatalog.server.model.ResourceType.METASTORE;
+
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.server.annotation.*;
+import io.unitycatalog.server.auth.UnityCatalogAuthorizer;
+import io.unitycatalog.server.auth.annotation.AuthorizeExpression;
+import io.unitycatalog.server.auth.annotation.AuthorizeKey;
+import io.unitycatalog.server.auth.decorator.UnityAccessEvaluator;
+import io.unitycatalog.server.auth.decorator.UnityAccessUtil;
 import io.unitycatalog.server.exception.GlobalExceptionHandler;
+import io.unitycatalog.server.model.CatalogInfo;
 import io.unitycatalog.server.model.CreateCatalog;
+import io.unitycatalog.server.model.ListCatalogsResponse;
+import io.unitycatalog.server.model.Privilege;
 import io.unitycatalog.server.model.UpdateCatalog;
 import io.unitycatalog.server.persist.CatalogRepository;
+import io.unitycatalog.server.persist.MetastoreRepository;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
+import lombok.SneakyThrows;
 
 @ExceptionHandler(GlobalExceptionHandler.class)
 public class CatalogService {
   private static final CatalogRepository CATALOG_REPOSITORY = CatalogRepository.getInstance();
+  private final UnityCatalogAuthorizer authorizer;
+  private final UnityAccessEvaluator evaluator;
 
-  public CatalogService() {}
+  @SneakyThrows
+  public CatalogService(UnityCatalogAuthorizer authorizer) {
+    this.authorizer = authorizer;
+    evaluator = new UnityAccessEvaluator(authorizer);
+  }
 
   @Post("")
+  @AuthorizeExpression("#authorizeAny(#principal, #metastore, METASTORE_ADMIN, CREATE_CATALOG)")
+  @AuthorizeKey(METASTORE)
   public HttpResponse createCatalog(CreateCatalog createCatalog) {
-    return HttpResponse.ofJson(CATALOG_REPOSITORY.addCatalog(createCatalog));
+    CatalogInfo catalogInfo = CATALOG_REPOSITORY.addCatalog(createCatalog);
+    initializeAuthorizations(catalogInfo);
+    return HttpResponse.ofJson(catalogInfo);
   }
 
   @Get("")
+  @AuthorizeExpression("#defer")
   public HttpResponse listCatalogs(
       @Param("max_results") Optional<Integer> maxResults,
       @Param("page_token") Optional<String> pageToken) {
-    return HttpResponse.ofJson(CATALOG_REPOSITORY.listCatalogs(maxResults, pageToken));
+    ListCatalogsResponse listCatalogsResponse =
+        CATALOG_REPOSITORY.listCatalogs(maxResults, pageToken);
+
+    filterCatalogs("""
+        #authorize(#principal, #metastore, METASTORE_ADMIN) ||
+        #authorizeAny(#principal, #catalog, OWNER, USE_CATALOG)
+        """,
+        listCatalogsResponse.getCatalogs());
+
+    return HttpResponse.ofJson(listCatalogsResponse);
   }
 
   @Get("/{name}")
-  public HttpResponse getCatalog(@Param("name") String name) {
+  @AuthorizeExpression("""
+      #authorize(#principal, #metastore, METASTORE_ADMIN) ||
+      #authorizeAny(#principal, #catalog, OWNER, USE_CATALOG)
+      """)
+  @AuthorizeKey(METASTORE)
+  public HttpResponse getCatalog(@Param("name") @AuthorizeKey(CATALOG) String name) {
     return HttpResponse.ofJson(CATALOG_REPOSITORY.getCatalog(name));
   }
 
   @Patch("/{name}")
-  public HttpResponse updateCatalog(@Param("name") String name, UpdateCatalog updateCatalog) {
+  @AuthorizeExpression("""
+      #authorize(#principal, #metastore, METASTORE_ADMIN) ||
+      #authorize(#principal, #catalog, OWNER)
+      """)
+  @AuthorizeKey(METASTORE)
+  public HttpResponse updateCatalog(
+      @Param("name") @AuthorizeKey(CATALOG) String name, UpdateCatalog updateCatalog) {
     return HttpResponse.ofJson(CATALOG_REPOSITORY.updateCatalog(name, updateCatalog));
   }
 
   @Delete("/{name}")
+  @AuthorizeExpression("""
+      #authorize(#principal, #metastore, METASTORE_ADMIN) ||
+      #authorize(#principal, #catalog, OWNER)
+      """)
+  @AuthorizeKey(METASTORE)
   public HttpResponse deleteCatalog(
-      @Param("name") String name, @Param("force") Optional<Boolean> force) {
+      @Param("name") @AuthorizeKey(CATALOG) String name, @Param("force") Optional<Boolean> force) {
+    CatalogInfo catalogInfo = CATALOG_REPOSITORY.getCatalog(name);
     CATALOG_REPOSITORY.deleteCatalog(name, force.orElse(false));
+    removeAuthorizations(catalogInfo);
     return HttpResponse.of(HttpStatus.OK);
+  }
+
+  public void filterCatalogs(String expression, List<CatalogInfo> entries) {
+    // TODO: would be nice to move this to filtering in the Decorator response
+    UUID principalId = UnityAccessUtil.findPrincipalId();
+
+    evaluator.filter(
+        principalId,
+        expression,
+        entries,
+        ci ->
+            Map.of(
+                METASTORE,
+                MetastoreRepository.getInstance().getMetastoreId(),
+                CATALOG,
+                UUID.fromString(ci.getId())));
+  }
+
+  private void initializeAuthorizations(CatalogInfo catalogInfo) {
+    UUID principalId = UnityAccessUtil.findPrincipalId();
+    authorizer.grantAuthorization(
+        principalId, UUID.fromString(catalogInfo.getId()), Privilege.OWNER);
+  }
+
+  private void removeAuthorizations(CatalogInfo catalogInfo) {
+    authorizer.clearAuthorizationsForResource(UUID.fromString(catalogInfo.getId()));
   }
 }
--- server/src/main/java/io/unitycatalog/server/service/TableService.java
+++ server/src/main/java/io/unitycatalog/server/service/TableService.java
@@ -1,36 +1,86 @@
 package io.unitycatalog.server.service;
 
+import static io.unitycatalog.server.model.ResourceType.CATALOG;
+import static io.unitycatalog.server.model.ResourceType.METASTORE;
+import static io.unitycatalog.server.model.ResourceType.SCHEMA;
+import static io.unitycatalog.server.model.ResourceType.TABLE;
+
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.server.annotation.*;
+import io.unitycatalog.server.auth.UnityCatalogAuthorizer;
+import io.unitycatalog.server.auth.annotation.AuthorizeExpression;
+import io.unitycatalog.server.auth.annotation.AuthorizeKey;
+import io.unitycatalog.server.auth.annotation.AuthorizeKeys;
+import io.unitycatalog.server.auth.decorator.UnityAccessEvaluator;
+import io.unitycatalog.server.auth.decorator.UnityAccessUtil;
 import io.unitycatalog.server.exception.GlobalExceptionHandler;
+import io.unitycatalog.server.model.CatalogInfo;
 import io.unitycatalog.server.model.CreateTable;
+import io.unitycatalog.server.model.ListTablesResponse;
+import io.unitycatalog.server.model.Privilege;
+import io.unitycatalog.server.model.SchemaInfo;
 import io.unitycatalog.server.model.TableInfo;
+import io.unitycatalog.server.persist.CatalogRepository;
+import io.unitycatalog.server.persist.MetastoreRepository;
+import io.unitycatalog.server.persist.SchemaRepository;
 import io.unitycatalog.server.persist.TableRepository;
+import lombok.SneakyThrows;
+
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 @ExceptionHandler(GlobalExceptionHandler.class)
 public class TableService {
 
   private static final TableRepository TABLE_REPOSITORY = TableRepository.getInstance();
+  private static final SchemaRepository SCHEMA_REPOSITORY = SchemaRepository.getInstance();
+  private static final CatalogRepository CATALOG_REPOSITORY = CatalogRepository.getInstance();
 
-  public TableService() {}
+  private final UnityCatalogAuthorizer authorizer;
+  private final UnityAccessEvaluator evaluator;
+
+  @SneakyThrows
+  public TableService(UnityCatalogAuthorizer authorizer) {
+    this.authorizer = authorizer;
+    evaluator = new UnityAccessEvaluator(authorizer);
+  }
 
   @Post("")
-  public HttpResponse createTable(CreateTable createTable) {
+  // The Databricks API does not have a create table endpoint, so defining sensible rules here.
+  @AuthorizeExpression("""
+      #authorizeAny(#principal, #catalog, OWNER, USE_CATALOG) && #authorizeAny(#principal, #schema, OWNER, USE_SCHEMA)
+      """)
+  public HttpResponse createTable(
+      @AuthorizeKeys({
+            @AuthorizeKey(value = SCHEMA, key = "schema_name"),
+            @AuthorizeKey(value = CATALOG, key = "catalog_name")
+          })
+          CreateTable createTable) {
     assert createTable != null;
-    TableInfo createTableResponse = TABLE_REPOSITORY.createTable(createTable);
-    return HttpResponse.ofJson(createTableResponse);
+    TableInfo tableInfo = TABLE_REPOSITORY.createTable(createTable);
+    initializeAuthorizations(tableInfo);
+    return HttpResponse.ofJson(tableInfo);
   }
 
   @Get("/{full_name}")
-  public HttpResponse getTable(@Param("full_name") String fullName) {
+  @AuthorizeExpression("""
+          #authorize(#principal, #metastore, METASTORE_ADMIN) ||
+          #authorize(#principal, #catalog, OWNER) ||
+          (#authorize(#principal, #schema, OWNER) && #authorize(#principal, #catalog, USE_CATALOG)) ||
+          (#authorize(#principal, #schema, USE_SCHEMA) && #authorize(#principal, #catalog, USE_CATALOG) && #authorizeAny(#principal, #table, OWNER, SELECT))
+          """)
+  @AuthorizeKey(METASTORE)
+  public HttpResponse getTable(@Param("full_name") @AuthorizeKey(TABLE) String fullName) {
     assert fullName != null;
     TableInfo tableInfo = TABLE_REPOSITORY.getTable(fullName);
     return HttpResponse.ofJson(tableInfo);
   }
 
   @Get("")
+  @AuthorizeExpression("#defer")
   public HttpResponse listTables(
       @Param("catalog_name") String catalogName,
       @Param("schema_name") String schemaName,
@@ -38,19 +88,79 @@ public class TableService {
       @Param("page_token") Optional<String> pageToken,
       @Param("omit_properties") Optional<Boolean> omitProperties,
       @Param("omit_columns") Optional<Boolean> omitColumns) {
-    return HttpResponse.ofJson(
-        TABLE_REPOSITORY.listTables(
+
+    ListTablesResponse listTablesResponse = TABLE_REPOSITORY.listTables(
             catalogName,
             schemaName,
             maxResults,
             pageToken,
             omitProperties.orElse(false),
-            omitColumns.orElse(false)));
+            omitColumns.orElse(false));
+
+    filterTables("""
+            #authorize(#principal, #metastore, METASTORE_ADMIN) ||
+            #authorize(#principal, #table, OWNER) ||
+            (#authorize(#principal, #table, SELECT) && #authorize(#principal, #schema, USE_SCHEMA) && #authorize(#principal, #catalog, USE_CATALOG))
+            """, listTablesResponse.getTables());
+
+    return HttpResponse.ofJson(listTablesResponse);
   }
 
   @Delete("/{full_name}")
-  public HttpResponse deleteTable(@Param("full_name") String fullName) {
+  @AuthorizeExpression("""
+          (#authorizeAll(#principal, #catalog, OWNER, USE_CATALOG) && #authorize(#principal, #schema, OWNER)) ||
+          #authorizeAll(#principal, #table, OWNER, USE_CATALOG, USE_SCHEMA)
+          """)
+  public HttpResponse deleteTable(@Param("full_name") @AuthorizeKey(TABLE) String fullName) {
+    TableInfo tableInfo = TABLE_REPOSITORY.getTable(fullName);
     TABLE_REPOSITORY.deleteTable(fullName);
+    removeAuthorizations(tableInfo);
     return HttpResponse.of(HttpStatus.OK);
+  }
+
+  public void filterTables(String expression, List<TableInfo> entries) {
+    // TODO: would be nice to move this to filtering in the Decorator response
+    UUID principalId = UnityAccessUtil.findPrincipalId();
+
+    evaluator.filter(
+            principalId,
+            expression,
+            entries,
+            ti -> {
+              CatalogInfo catalogInfo = CATALOG_REPOSITORY.getCatalog(ti.getCatalogName());
+              SchemaInfo schemaInfo =
+                      SCHEMA_REPOSITORY.getSchema(ti.getCatalogName() + "." + ti.getSchemaName());
+              return Map.of(
+                      METASTORE,
+                      MetastoreRepository.getInstance().getMetastoreId(),
+                      CATALOG,
+                      UUID.fromString(catalogInfo.getId()),
+                      SCHEMA,
+                      UUID.fromString(schemaInfo.getSchemaId()),
+                      TABLE,
+                      UUID.fromString(ti.getTableId()));
+            });
+  }
+
+  private void initializeAuthorizations(TableInfo tableInfo) {
+    SchemaInfo schemaInfo =
+        SCHEMA_REPOSITORY.getSchema(tableInfo.getCatalogName() + "." + tableInfo.getSchemaName());
+    UUID principalId = UnityAccessUtil.findPrincipalId();
+    // add owner privilege
+    authorizer.grantAuthorization(
+        principalId, UUID.fromString(tableInfo.getTableId()), Privilege.OWNER);
+    // make table a child of the schema
+    authorizer.addHierarchyChild(
+        UUID.fromString(schemaInfo.getSchemaId()), UUID.fromString(tableInfo.getTableId()));
+  }
+
+  private void removeAuthorizations(TableInfo tableInfo) {
+    SchemaInfo schemaInfo =
+        SCHEMA_REPOSITORY.getSchema(tableInfo.getCatalogName() + "." + tableInfo.getSchemaName());
+    // remove any direct authorizations on the table
+    authorizer.clearAuthorizationsForResource(UUID.fromString(tableInfo.getTableId()));
+    // remove link to the parent schema
+    authorizer.removeHierarchyChild(
+        UUID.fromString(schemaInfo.getSchemaId()), UUID.fromString(tableInfo.getTableId()));
   }
 }
