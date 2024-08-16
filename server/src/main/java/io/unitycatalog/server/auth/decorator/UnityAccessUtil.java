cfc4f012e52713b139f305c2e6b452251fd71024
Joseph Mocker <mock@fakebelieve.org>
2024-08-16 08:45:56 -0700

    The JWT `sub` is the email address, so look up user's UUID from it.

--- server/src/main/java/io/unitycatalog/server/auth/decorator/UnityAccessUtil.java
+++ server/src/main/java/io/unitycatalog/server/auth/decorator/UnityAccessUtil.java
@@ -3,13 +3,18 @@ package io.unitycatalog.server.auth.decorator;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.linecorp.armeria.server.ServiceRequestContext;
+import io.unitycatalog.server.persist.UserRepository;
 import java.util.UUID;
 
 public class UnityAccessUtil {
+
+  private static final UserRepository USER_REPOSITORY = UserRepository.getInstance();
+
   public static UUID findPrincipalId() {
     ServiceRequestContext ctx = ServiceRequestContext.current();
     DecodedJWT decodedJWT = ctx.attr(TemporaryIdentityDecorator.DECODED_JWT_ATTR);
     Claim sub = decodedJWT.getClaim("sub");
-    return UUID.fromString(sub.asString());
+
+    return UUID.fromString(USER_REPOSITORY.getUserByEmail(sub.asString()).getId());
   }
 }
