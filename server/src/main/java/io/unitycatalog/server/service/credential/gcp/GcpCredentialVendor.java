9e3ebaa177e5f3359986f5ed1fb4043410f3394d
Alex Reid <alex.reid@databricks.com>
2024-08-22 11:56:38 -0700

    implementation for basic credential vending (#274)
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [x] If there is a related issue, make sure it is linked to this PR.
    - [ ] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    This PR adds the ability to vend temporary credentials for S3, Azure,
    and Google Cloud AND is able to downscope the temporary credentials to
    access only certain paths and certain privileges (read vs. write, etc.).
    This also updates the Iceberg REST load table implementation to vend a
    temporary credential and supply it in the table config so client can
    read table without needing to have creds set on client.
    
    AWS/S3 setup:
    
    Bucket + Role + Creds (downscoping)
    - Create S3 bucket
    - Create an AWS IAM role that has access to s3 bucket
    - Create user and retrieve access keys
    - Give user permission to assume role
    - Update IAM role trust policy to trust newly created user
    
    Set the following properties in server.properties file
    
    ```
    s3.bucketPath.X=s3://bucket-where-tables-live
    s3.region.X=aws-region-of-bucket (e.g. us-east-1)
    s3.awsRoleArn.X=IamRole (role to assume for access to bucket)
    # Optional (If blank, it will use DefaultCredentialsProviderChain)
    s3.accessKey.X=<access-key>
    s3.secretKey.X=<secret-key>
    ```
    NOTE: If you don't set keys in the server.properties, you must configure
    something on
    [DefaultCredentialsProviderChain](https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/credentials-chain.html)
    that the sts client will use to assume the provided role.
    
    AWS Session Creds (no downscoping)
    NOTE: this should be used for test purposes only
    - Create temporary session and supply access key, secret key, and
    session token
    
    Azure Setup:
    - Create Storage Account and container
    - Create service principal 
    - Update role permissions on storage account and give service principal
    access (e.g. Storage Blob Data Reader, Storage Blob Data Contributor,
    Admin, etc.)
    - Retrieve creds for service principal
    
    Set the following properties in server.properties file
    
    ```
    adls.storageAccountName.X=
    adls.tenantId.X=
    adls.clientId.X=
    adls.clientSecret.X=
    ```
    
    Google Cloud Setup:
    - Create GCS Bucket
    - Create Service Account
    - Grant access to new service account to new bucket (e.g. assign Storage
    Object Admin role to service account)
    - Create and retrieve keys for Service Account
    -- This will be a json key file, download it and put it somewhere
    accessible by the UC server
    
    ```
    gcs.bucketPath.X=gs://bucket-name
    gcs.jsonKeyFilePath.X=</path/to/json/key/file>
    ```
    
    This was tested by using OSS Spark to read iceberg/uniform tables
    created in all 3 cloud providers.
    
    Follow PR to come shortly that provides full documentation on setup,
    etc.
    Better integration tests to follow as well.
    Storage configurations should at some point become entities and move
    into the db.
    
    Addresses Issues:
    https://github.com/unitycatalog/unitycatalog/issues/230
    https://github.com/unitycatalog/unitycatalog/issues/47
    https://github.com/unitycatalog/unitycatalog/issues/105

---
+++ server/src/main/java/io/unitycatalog/server/service/credential/gcp/GcpCredentialVendor.java
@@ -0,0 +1,94 @@
+package io.unitycatalog.server.service.credential.gcp;
+
+import static java.lang.String.format;
+
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.CredentialAccessBoundary;
+import com.google.auth.oauth2.DownscopedCredentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.OAuth2Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.common.base.CharMatcher;
+import io.unitycatalog.server.persist.utils.ServerPropertiesUtils;
+import io.unitycatalog.server.service.credential.CredentialContext;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import lombok.SneakyThrows;
+import org.apache.iceberg.Files;
+
+public class GcpCredentialVendor {
+
+  public static final List<String> INITIAL_SCOPES =
+      List.of("https://www.googleapis.com/auth/cloud-platform");
+
+  private final Map<String, String> gcsConfigurations;
+
+  public GcpCredentialVendor() {
+    this.gcsConfigurations = ServerPropertiesUtils.getInstance().getGcsConfigurations();
+  }
+
+  @SneakyThrows
+  public AccessToken vendGcpToken(CredentialContext credentialContext) {
+    String serviceAccountKeyJsonFilePath =
+        gcsConfigurations.get(credentialContext.getStorageBase());
+
+    GoogleCredentials creds;
+    if (serviceAccountKeyJsonFilePath != null && !serviceAccountKeyJsonFilePath.isEmpty()) {
+      creds =
+          ServiceAccountCredentials.fromStream(
+              Files.localInput(serviceAccountKeyJsonFilePath).newStream());
+    } else {
+      creds = GoogleCredentials.getApplicationDefault();
+    }
+
+    return downscopeGcpCreds(creds.createScoped(INITIAL_SCOPES), credentialContext)
+        .refreshAccessToken();
+  }
+
+  OAuth2Credentials downscopeGcpCreds(GoogleCredentials credentials, CredentialContext context) {
+    CredentialAccessBoundary.Builder boundaryBuilder = CredentialAccessBoundary.newBuilder();
+    List<String> roles = resolvePrivilegesToRoles(context.getPrivileges());
+
+    context
+        .getLocations()
+        .forEach(
+            location -> {
+              URI locationUri = URI.create(location);
+              String path = CharMatcher.is('/').trimLeadingFrom(locationUri.getPath());
+
+              String resource =
+                  format("//storage.googleapis.com/projects/_/buckets/%s", locationUri.getHost());
+              String expr =
+                  format(
+                      "resource.name.startsWith('projects/_/buckets/%s/objects/%s')",
+                      locationUri.getHost(), path);
+
+              boundaryBuilder.addRule(
+                  CredentialAccessBoundary.AccessBoundaryRule.newBuilder()
+                      .setAvailablePermissions(roles)
+                      .setAvailabilityCondition(
+                          CredentialAccessBoundary.AccessBoundaryRule.AvailabilityCondition
+                              .newBuilder()
+                              .setExpression(expr)
+                              .build())
+                      .setAvailableResource(resource)
+                      .build());
+            });
+
+    return DownscopedCredentials.newBuilder()
+        .setSourceCredential(credentials)
+        .setCredentialAccessBoundary(boundaryBuilder.build())
+        .build();
+  }
+
+  List<String> resolvePrivilegesToRoles(Set<CredentialContext.Privilege> privileges) {
+    if (privileges.contains(CredentialContext.Privilege.UPDATE)) {
+      return List.of("inRole:roles/storage.objectAdmin");
+    } else if (privileges.contains(CredentialContext.Privilege.SELECT)) {
+      return List.of("inRole:roles/storage.objectViewer");
+    }
+    return List.of();
+  }
+}
