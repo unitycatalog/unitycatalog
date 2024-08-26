6f99ca5f904df549e40a695a37f1f3a91f2da763
Joseph Mocker <mock@fakebelieve.org>
2024-08-26 16:03:12 -0700

    javadoc comments.

--- server/src/main/java/io/unitycatalog/server/auth/annotation/AuthorizeKeys.java
+++ server/src/main/java/io/unitycatalog/server/auth/annotation/AuthorizeKeys.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Map multiple AuthorizeKeys to a request payload parameter.
+ *
+ * <p>In some cases, it may be necessary to authorize a request based on multiple keys in the
+ * request payload object. In order to do this, you would define multiple AuthorizeKey annotations
+ * in the service method signature by using this annotation.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.PARAMETER})
 public @interface AuthorizeKeys {
