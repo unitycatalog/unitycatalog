87ba68448534ac54265a63b989fc246cdfe6ad6a
Bijan Houle <bijanhoule@users.noreply.github.com>
2024-08-29 15:59:04 -0600

    add table create/drop for local FS delta

--- integration-tests/src/test/java/io/unitycatalog/integrationtests/SparkLocalTest.java
+++ integration-tests/src/test/java/io/unitycatalog/integrationtests/SparkLocalTest.java
@@ -7,8 +7,10 @@ import org.apache.spark.sql.SparkSession;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
 import java.util.List;
 
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SparkLocalTest extends BaseSparkTest {
@@ -38,5 +40,28 @@ public class SparkLocalTest extends BaseSparkTest {
         List<String> actual = spark.sql("SELECT * FROM default.numbers_parquet").toJSON().collectAsList();
         List<String> expect = getExpectData("read_numbers.json");
         assertThat(actual).isEqualTo(expect);
+    }
+
+    @SneakyThrows
+    @Test
+    public void deltaCRUD() {
+        // todo: break this up a bit / split into multiple tests
+        String SCHEMA = "test_schema";
+        String TABLE = "test_table";
+
+        String tmpdir = Files.createTempDirectory("uc-test-table").toFile().getAbsolutePath();
+        try {
+            spark.sql(format("CREATE SCHEMA %s", SCHEMA));
+            spark.sql(format("CREATE TABLE %s.%s (id INT) USING DELTA LOCATION '%s'", SCHEMA, TABLE, tmpdir));
+            spark.sql(format("DELETE FROM %s.%s", SCHEMA, TABLE));
+            spark.sql(format("INSERT INTO %s.%s VALUES (1),(2),(3)", SCHEMA, TABLE));
+            List<String> actual = spark.sql(format("SELECT * FROM %s.%s ORDER BY 1", SCHEMA, TABLE)).toJSON().collectAsList();
+            List<String> expect = List.of("{\"id\":1}", "{\"id\":2}", "{\"id\":3}");
+            assertThat(actual).isEqualTo(expect);
+        } finally {
+            spark.sql(format("DROP TABLE IF EXISTS %s.%s", SCHEMA, TABLE));
+            spark.sql(format("DROP SCHEMA IF EXISTS %s", SCHEMA));
+        }
+
     }
 }
