b287bafad5cd6896b0e99d085d22a49cbc7c2795
Joseph Mocker <mock@fakebelieve.org>
2024-08-29 10:34:54 -0700

    Added some auth and user tests.

--- examples/cli/src/test/java/io/unitycatalog/cli/auth/CliAuthCrudTest.java
+++ examples/cli/src/test/java/io/unitycatalog/cli/auth/CliAuthCrudTest.java
@@ -1,17 +1,25 @@
 package io.unitycatalog.cli.auth;
 
+import com.auth0.jwt.JWT;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.unitycatalog.server.base.auth.BaseAuthCRUDTest;
+import io.unitycatalog.server.security.JwtClaim;
+import io.unitycatalog.server.security.JwtTokenType;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 import static io.unitycatalog.cli.TestUtils.addServerAndAuthParams;
 import static io.unitycatalog.cli.TestUtils.executeCLICommand;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import io.unitycatalog.server.base.auth.BaseAuthCRUDTest;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import org.junit.jupiter.api.Test;
 
 public class CliAuthCrudTest extends BaseAuthCRUDTest {
 
@@ -33,7 +41,8 @@ public class CliAuthCrudTest extends BaseAuthCRUDTest {
   }
 
   @Test
-  public void testAuthAccess() throws IOException {
+  public void testAuthAccess()
+      throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
     System.out.println("Testing access..");
 
     List<String> argsList = List.of("catalog", "list");
@@ -42,8 +51,8 @@ public class CliAuthCrudTest extends BaseAuthCRUDTest {
     assertThrows(
         RuntimeException.class,
         () -> {
-          String[] args = addServerAndAuthParams(argsList, serverConfig);
-          JsonNode catalogInfoJson = executeCLICommand(args);
+          String[] localArgs = addServerAndAuthParams(argsList, serverConfig);
+          JsonNode localResultJson = executeCLICommand(localArgs);
         });
 
     // Test with Authentication on authenticated end point
@@ -54,6 +63,51 @@ public class CliAuthCrudTest extends BaseAuthCRUDTest {
 
     String[] args = addServerAndAuthParams(argsList, serverConfig);
     JsonNode catalogInfoJson = executeCLICommand(args);
+    assertNotNull(catalogInfoJson);
+
+    // Test with authentication on authenticated end point with missing user
+    String missingUserJwt =
+        JWT.create()
+            .withSubject(securityContext.getServiceName())
+            .withIssuer(securityContext.getLocalIssuer())
+            .withIssuedAt(new Date())
+            .withKeyId(securityConfiguration.getKeyId())
+            .withJWTId(UUID.randomUUID().toString())
+            .withClaim(JwtClaim.TOKEN_TYPE.key(), JwtTokenType.ACCESS.name())
+            .withClaim(JwtClaim.SUBJECT.key(), "missing@localhost")
+            .sign(securityConfiguration.algorithmRSA());
+
+    serverConfig.setAuthToken(missingUserJwt);
+
+    assertThrows(
+        RuntimeException.class,
+        () -> {
+          String[] localArgs = addServerAndAuthParams(argsList, serverConfig);
+          JsonNode localResultJson = executeCLICommand(localArgs);
+        });
+
+    // Test adding a user
+    List<String> argsAddUser =
+        List.of("user", "create", "--email", "test@localhost", "--name", "Test User");
+    args = addServerAndAuthParams(argsAddUser, serverConfig);
+    catalogInfoJson = executeCLICommand(args);
+    assertNotNull(catalogInfoJson);
+
+    // Test with authentication on authenticated end point with added user
+    String testUserJwt =
+        JWT.create()
+            .withSubject(securityContext.getServiceName())
+            .withIssuer(securityContext.getLocalIssuer())
+            .withIssuedAt(new Date())
+            .withKeyId(securityConfiguration.getKeyId())
+            .withJWTId(UUID.randomUUID().toString())
+            .withClaim(JwtClaim.TOKEN_TYPE.key(), JwtTokenType.ACCESS.name())
+            .withClaim(JwtClaim.SUBJECT.key(), "test@localhost")
+            .sign(securityConfiguration.algorithmRSA());
+
+    serverConfig.setAuthToken(testUserJwt);
+    args = addServerAndAuthParams(argsList, serverConfig);
+    catalogInfoJson = executeCLICommand(args);
     assertNotNull(catalogInfoJson);
   }
 }
--- server/src/test/java/io/unitycatalog/server/base/auth/BaseAuthCRUDTest.java
+++ server/src/test/java/io/unitycatalog/server/base/auth/BaseAuthCRUDTest.java
@@ -1,15 +1,30 @@
 package io.unitycatalog.server.base.auth;
 
 import io.unitycatalog.server.base.BaseServerTest;
+import io.unitycatalog.server.security.SecurityConfiguration;
+import io.unitycatalog.server.security.SecurityContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.nio.file.Path;
+
+import static io.unitycatalog.server.security.SecurityContext.Issuers.INTERNAL;
+
 public abstract class BaseAuthCRUDTest extends BaseServerTest {
+
+  protected SecurityConfiguration securityConfiguration;
+  protected SecurityContext securityContext;
 
   @BeforeEach
   public void setUp() {
     System.setProperty("server.authorization", "enable");
     super.setUp();
+
+    Path configurationFolder = Path.of("etc", "conf");
+
+    securityConfiguration = new SecurityConfiguration(configurationFolder);
+    securityContext =
+        new SecurityContext(configurationFolder, securityConfiguration, "server", INTERNAL);
   }
 
   @AfterEach
