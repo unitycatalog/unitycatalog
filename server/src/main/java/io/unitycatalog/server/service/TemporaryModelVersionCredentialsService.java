e3c7e7afd9fa1d0ae7951549d89e81d4ca0d3469
Kris Concepcion <84737625+kriscon-db@users.noreply.github.com>
2024-09-02 00:53:49 -0400

    Model version credential handler (#384)
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [ ] If there is a related issue, make sure it is linked to this PR.
    - [ ] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    Added the model version credential handler.
    
    <!-- Please state what you've changed and how it might affect the users.
    -->

---
+++ server/src/main/java/io/unitycatalog/server/service/TemporaryModelVersionCredentialsService.java
@@ -0,0 +1,68 @@
+package io.unitycatalog.server.service;
+
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.annotation.ExceptionHandler;
+import com.linecorp.armeria.server.annotation.Post;
+import io.unitycatalog.server.exception.BaseException;
+import io.unitycatalog.server.exception.ErrorCode;
+import io.unitycatalog.server.exception.GlobalExceptionHandler;
+import io.unitycatalog.server.model.*;
+import io.unitycatalog.server.persist.ModelRepository;
+import io.unitycatalog.server.persist.utils.RepositoryUtils;
+import io.unitycatalog.server.service.credential.CredentialOperations;
+import io.unitycatalog.server.service.credential.CredentialContext;
+import io.unitycatalog.server.persist.ModelRepository;
+
+import java.util.Collections;
+import java.util.Set;
+
+import static io.unitycatalog.server.service.credential.CredentialContext.Privilege.SELECT;
+import static io.unitycatalog.server.service.credential.CredentialContext.Privilege.UPDATE;
+
+@ExceptionHandler(GlobalExceptionHandler.class)
+public class TemporaryModelVersionCredentialsService {
+
+    private static final ModelRepository MODEL_REPOSITORY = ModelRepository.getInstance();
+
+    private final CredentialOperations credentialOps;
+
+    public TemporaryModelVersionCredentialsService(CredentialOperations credentialOps) {
+        this.credentialOps = credentialOps;
+    }
+
+    @Post("")
+    public HttpResponse generateTemporaryModelVersionCredentials(
+            GenerateTemporaryModelVersionCredentials generateTemporaryModelVersionCredentials) {
+
+        long modelVersion = generateTemporaryModelVersionCredentials.getVersion();
+        String catalogName = generateTemporaryModelVersionCredentials.getCatalogName();
+        String schemaName = generateTemporaryModelVersionCredentials.getSchemaName();
+        String modelName = generateTemporaryModelVersionCredentials.getModelName();
+        String fullName = RepositoryUtils.getAssetFullName(catalogName, schemaName, modelName);
+
+        ModelVersionInfo modelVersionInfo = MODEL_REPOSITORY.getModelVersion(fullName, modelVersion);
+        String storageLocation = modelVersionInfo.getStorageLocation();
+        if (storageLocation.toLowerCase().startsWith("file")) {
+            throw new BaseException(ErrorCode.INVALID_ARGUMENT, "Cannot request credentials on a model version with a file based storage location: " + fullName + "/" + modelVersion);
+        }
+        ModelVersionOperation requestedOperation = generateTemporaryModelVersionCredentials.getOperation();
+        // Must enforce that the status of the model version matches the requested credential type.
+        if (modelVersionInfo.getStatus() == ModelVersionStatus.FAILED_REGISTRATION || modelVersionInfo.getStatus() == ModelVersionStatus.MODEL_VERSION_STATUS_UNKNOWN) {
+            throw new BaseException(ErrorCode.INVALID_ARGUMENT, "Cannot request credentials on a model version with status " + modelVersionInfo.getStatus().getValue() + ": " + fullName + "/" + modelVersion);
+        }
+        if ((modelVersionInfo.getStatus() != ModelVersionStatus.PENDING_REGISTRATION &&
+                requestedOperation == ModelVersionOperation.READ_WRITE_MODEL_VERSION)) {
+            throw new BaseException(ErrorCode.INVALID_ARGUMENT, "Cannot request read/write credentials on a model version that has been finalized: " + fullName + "/" + modelVersion);
+        }
+        return HttpResponse.ofJson(credentialOps.vendCredentialForModelVersion(modelVersionInfo, modelVersionOperationToPrivileges(requestedOperation)));
+    }
+
+    private Set<CredentialContext.Privilege> modelVersionOperationToPrivileges(ModelVersionOperation modelVersionOperation) {
+        return switch (modelVersionOperation) {
+            case READ_MODEL_VERSION -> Set.of(SELECT);
+            case READ_WRITE_MODEL_VERSION -> Set.of(SELECT, UPDATE);
+            case UNKNOWN_MODEL_VERSION_OPERATION ->
+                    throw new BaseException(ErrorCode.INVALID_ARGUMENT, "Unknown operation in the request: " + ModelVersionOperation.UNKNOWN_MODEL_VERSION_OPERATION);
+        };
+    }
+}
