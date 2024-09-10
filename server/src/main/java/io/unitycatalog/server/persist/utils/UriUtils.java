515d15aa8d43e51879dd171a63b6ae447e7daa98
Kris Concepcion <84737625+kriscon-db@users.noreply.github.com>
2024-09-10 11:18:04 -0400

    Javafmt fix for UriUtils (#458)
    **PR Checklist**
    
    - [ ] A description of the changes is added to the description of this
    PR.
    - [ ] If there is a related issue, make sure it is linked to this PR.
    - [ ] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    Run javafmt on UriUtils

--- server/src/main/java/io/unitycatalog/server/persist/utils/UriUtils.java
+++ server/src/main/java/io/unitycatalog/server/persist/utils/UriUtils.java
@@ -39,9 +39,9 @@ public class UriUtils {
   private static String modelStorageRootPropertyCached;
 
   /**
-   * TODO: Deprecate this method once unit tests are self contained and this class gets re-instantiated with
-   *       each test.  Property updates shouldn't affect the instantiated class and we should require a server
-   *       restart if the properties file is updated.
+   * TODO: Deprecate this method once unit tests are self contained and this class gets
+   * re-instantiated with each test. Property updates shouldn't affect the instantiated class and we
+   * should require a server restart if the properties file is updated.
    */
   private static void reset() {
     modelStorageRootPropertyCached = null;
