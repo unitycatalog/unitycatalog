136eae0c33d1021da21721c354345774aad9e272
Vikrant Puppala <vikrant.puppala@databricks.com>
2024-10-15 10:49:21 +0530

    test for server restart
    Signed-off-by: Vikrant Puppala <vikrant.puppala@databricks.com>

--- server/src/test/java/io/unitycatalog/server/base/metastore/BaseMetastoreSummaryTest.java
+++ server/src/test/java/io/unitycatalog/server/base/metastore/BaseMetastoreSummaryTest.java
@@ -26,9 +26,17 @@ public abstract class BaseMetastoreSummaryTest extends BaseCRUDTest {
     GetMetastoreSummaryResponse metastoreSummary = metastoreOperations.getMetastoreSummary();
     String metastoreId1 = metastoreSummary.getMetastoreId();
     assertThat(metastoreId1).isNotNull();
-    // Verify that it is stable
-    String metastoreId2 = metastoreSummary.getMetastoreId();
+    // Verify that the metastore ID is stable
+    GetMetastoreSummaryResponse metastoreSummary2 = metastoreOperations.getMetastoreSummary();
+    String metastoreId2 = metastoreSummary2.getMetastoreId();
     assertThat(metastoreId2).isNotNull();
     assertThat(metastoreId1).isEqualTo(metastoreId2);
+    // Verify that the metastore ID is stable even after a server restart
+    unityCatalogServer.stop();
+    unityCatalogServer.start();
+    GetMetastoreSummaryResponse metastoreSummary3 = metastoreOperations.getMetastoreSummary();
+    String metastoreId3 = metastoreSummary3.getMetastoreId();
+    assertThat(metastoreId3).isNotNull();
+    assertThat(metastoreId1).isEqualTo(metastoreId3);
   }
 }
