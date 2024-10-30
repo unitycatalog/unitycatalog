3fd1b0c39f3ab80d645d7228e1cc02aa6e2f3b8c
Michael Berk <michaelberk99@gmail.com>
2024-10-30 08:31:05 -0400

    Update ai/integrations/crewai/src/unitycatalog/ai/crewai/toolkit.py
    Co-authored-by: Ben Wilson <39283302+BenWilson2@users.noreply.github.com>
    Signed-off-by: Michael Berk <michaelberk99@gmail.com>

--- ai/integrations/crewai/src/unitycatalog/ai/crewai/toolkit.py
+++ ai/integrations/crewai/src/unitycatalog/ai/crewai/toolkit.py
@@ -55,7 +55,7 @@ class UCFunctionToolkit(BaseModel):
         tools_dict (Dict[str, FunctionTool]): A dictionary mapping function names to their
             corresponding tools.
         client (Optional[BaseFunctionClient]): The client used to manage functions.
-        description (Optional[Bool]): Flag to check if the description has been updated.
+        description_updated (Optional[Bool]): Flag to check if the description has been updated.
         cache_function (Optional[Callable]): Function that will be used to determine if the tool
             should be cached, should return a boolean. If None, the tool will be cached.
         result_as_answer (Optional[Bool]): Flag to check if the tool should be the final
