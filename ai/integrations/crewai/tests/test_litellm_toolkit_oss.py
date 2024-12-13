08da0cee2a74bf9e3f327a17c35dff1c6c7ff15d
Michael Berk <michaelberk99@gmail.com>
2024-12-13 12:34:31 -0500

    OSS integration test
    Signed-off-by: Michael Berk <michaelberk99@gmail.com>

--- ai/integrations/crewai/tests/test_litellm_toolkit_oss.py
+++ ai/integrations/crewai/tests/test_litellm_toolkit_oss.py
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-=======
 import json
->>>>>>> 44175c2 (OSS integration test)
 import os
 from unittest import mock
 
 import pytest
 import pytest_asyncio
-from pydantic import ValidationError
+from autogen import ConversableAgent
 
+from unitycatalog.ai.autogen.toolkit import AutogenTool, UCFunctionToolkit
 from unitycatalog.ai.core.base import FunctionExecutionResult
 from unitycatalog.ai.core.client import UnitycatalogFunctionClient
-<<<<<<< HEAD
-from unitycatalog.ai.litellm.toolkit import LiteLLMTool, UCFunctionToolkit
-=======
-from unitycatalog.ai.litellm.toolkit import UCFunctionToolkit
->>>>>>> 44175c2 (OSS integration test)
 from unitycatalog.ai.test_utils.function_utils_oss import (
     CATALOG,
     create_function_and_cleanup_oss,
@@ -28,12 +21,29 @@ from unitycatalog.client import (
     FunctionParameterInfos,
 )
 
-SCHEMA = os.environ.get("SCHEMA", "ucai_litellm_test")
+try:
+    # v2
+    from pydantic_core._pydantic_core import ValidationError
+except ImportError:
+    # v1
+    from pydantic.error_wrappers import ValidationError
+
+SCHEMA = os.environ.get("SCHEMA", "ucai_autogen_test")
+
+
+def mock_autogen_tool_response(function_name, input_data, message_id):
+    input_data["code"] = 'print("Hello, World!")'
+
+    return {
+        "id": message_id,
+        "type": "function_call",
+        "name": function_name,
+        "arguments": input_data,
+    }
 
 
 @pytest_asyncio.fixture
 async def uc_client():
-    """Fixture to create and yield an OSS Unity Catalog client."""
     config = Configuration()
     config.host = "http://localhost:8080/api/2.1/unity-catalog"
     uc_api_client = ApiClient(configuration=config)
@@ -48,6 +58,60 @@ async def uc_client():
     uc_api_client.close()
 
 
+@pytest.fixture
+def sample_autogen_tool():
+    fn = mock.MagicMock()
+    name = "sample_function"
+    description = "A sample function for testing."
+    tool = {
+        "type": "function",
+        "function": {
+            "name": name,
+            "strict": True,
+            "parameters": {
+                "properties": {
+                    "location": {
+                        "anyOf": [{"type": "string"}, {"type": "null"}],
+                        "description": "Retrieves the current weather from a provided location.",
+                        "title": "Location",
+                    }
+                },
+                "type": "object",
+                "additionalProperties": False,
+                "required": ["location"],
+            },
+            "description": description,
+        },
+    }
+
+    return AutogenTool(fn=fn, name=name, description=description, tool=tool)
+
+
+def test_autogen_tool_to_dict(sample_autogen_tool):
+    expected_output = {
+        "name": sample_autogen_tool.name,
+        "description": sample_autogen_tool.description,
+        "tool": sample_autogen_tool.tool,
+    }
+    assert sample_autogen_tool.to_dict() == expected_output
+
+
+def test_autogen_tool_register_function(sample_autogen_tool):
+    mock_caller = mock.MagicMock(spec=ConversableAgent)
+    mock_executor = mock.MagicMock(spec=ConversableAgent)
+    mock_executor._wrap_function.return_value = "wrapped_function"
+
+    sample_autogen_tool.register_function(callers=mock_caller, executors=mock_executor)
+
+    mock_caller.update_tool_signature.assert_called_once_with(
+        sample_autogen_tool.tool, is_remove=False
+    )
+    mock_executor._wrap_function.assert_called_once_with(sample_autogen_tool.fn)
+    mock_executor.register_function.assert_called_once_with(
+        {sample_autogen_tool.name: "wrapped_function"}
+    )
+
+
 @pytest.mark.asyncio
 async def test_toolkit_e2e(uc_client):
     with create_function_and_cleanup_oss(uc_client, schema=SCHEMA) as func_obj:
@@ -55,17 +119,11 @@ async def test_toolkit_e2e(uc_client):
         tools = toolkit.tools
         assert len(tools) == 1
         tool = tools[0]
-<<<<<<< HEAD
         assert func_obj.comment in tool.description
-=======
-        assert func_obj.full_function_name.replace(".", "__") in tool.description
-        assert func_obj.comment in tool.description
-        assert tool.client_config == uc_client.to_dict()
 
         input_args = {"code": "print(1)"}
         result = json.loads(tool.fn(**input_args))["value"]
         assert result == "1\n"
->>>>>>> 44175c2 (OSS integration test)
 
         toolkit = UCFunctionToolkit(
             function_names=[f.full_name for f in uc_client.list_functions(CATALOG, SCHEMA)],
@@ -83,23 +141,13 @@ async def test_toolkit_e2e_manually_passing_client(uc_client):
         assert len(tools) == 1
         tool = tools[0]
         assert tool.name == func_obj.tool_name
-<<<<<<< HEAD
         assert func_obj.comment in tool.description
-=======
-        assert func_obj.full_function_name.replace(".", "__") in tool.description
-        assert func_obj.comment in tool.description
-        assert (
-            "{'code': {'description': 'Python code to execute. Remember to print the final result to stdout.'"
-            in tool.description
-        )
-        assert tool.client_config == uc_client.to_dict()
         input_args = {"code": "print(1)"}
         result = json.loads(tool.fn(**input_args))["value"]
         assert result == "1\n"
->>>>>>> 44175c2 (OSS integration test)
 
         toolkit = UCFunctionToolkit(
-            function_names=[f"{CATALOG}.{SCHEMA}.*"],
+            function_names=[f.full_name for f in uc_client.list_functions(CATALOG, SCHEMA)],
             client=uc_client,
         )
         assert len(toolkit.tools) >= 1
@@ -110,23 +158,19 @@ async def test_toolkit_e2e_manually_passing_client(uc_client):
 async def test_multiple_toolkits(uc_client):
     with create_function_and_cleanup_oss(uc_client, schema=SCHEMA) as func_obj:
         toolkit1 = UCFunctionToolkit(function_names=[func_obj.full_function_name], client=uc_client)
-        toolkit2 = UCFunctionToolkit(function_names=[f"{CATALOG}.{SCHEMA}.*"], client=uc_client)
-<<<<<<< HEAD
-        toolkits = [toolkit1, toolkit2]
-
-        assert all(isinstance(toolkit.tools[0], LiteLLMTool) for toolkit in toolkits)
-=======
+        toolkit2 = UCFunctionToolkit(
+            function_names=[f.full_name for f in uc_client.list_functions(CATALOG, SCHEMA)],
+            client=uc_client,
+        )
         tool1 = toolkit1.tools[0]
         tool2 = [t for t in toolkit2.tools if t.name == func_obj.tool_name][0]
         input_args = {"code": "print(1)"}
         result1 = json.loads(tool1.fn(**input_args))["value"]
         result2 = json.loads(tool2.fn(**input_args))["value"]
         assert result1 == result2
->>>>>>> 44175c2 (OSS integration test)
 
 
 def test_toolkit_creation_errors_no_client():
-    """Test that UCFunctionToolkit raises ValidationError when no client is provided."""
     with pytest.raises(
         ValidationError,
         match=r"No client provided, either set the client when creating a toolkit or set the default client",
@@ -135,16 +179,11 @@ def test_toolkit_creation_errors_no_client():
 
 
 def test_toolkit_creation_errors_invalid_client(uc_client):
-    """Test that UCFunctionToolkit raises ValidationError when an invalid client is provided."""
-    with pytest.raises(
-        ValidationError,
-        match=r"Input should be an instance of BaseFunctionClient",
-    ):
+    with pytest.raises(ValidationError, match=r"Input should be an instance of BaseFunctionClient"):
         UCFunctionToolkit(function_names=[], client="client")
 
 
 def test_toolkit_creation_errors_missing_function_names(uc_client):
-    """Test that UCFunctionToolkit raises ValidationError when function_names are missing."""
     with pytest.raises(
         ValidationError,
         match=r"Cannot create tool instances without function_names being provided.",
@@ -153,16 +192,14 @@ def test_toolkit_creation_errors_missing_function_names(uc_client):
 
 
 def test_toolkit_function_argument_errors(uc_client):
-    """Test that UCFunctionToolkit raises ValidationError when function_names are not provided."""
     with pytest.raises(
         ValidationError,
-        match=r"Cannot create tool instances without function_names being provided.",
+        match=r"1 validation error for UCFunctionToolkit\nfunction_names\n  Field required",
     ):
         UCFunctionToolkit(client=uc_client)
 
 
 def generate_function_info():
-    """Generate a mock FunctionInfo object for testing."""
     parameters = [
         {
             "name": "x",
@@ -183,14 +220,13 @@ def generate_function_info():
         input_params=FunctionParameterInfos(
             parameters=[FunctionParameterInfo(**param) for param in parameters]
         ),
-        full_name="catalog.schema.test",
+        full_name=f"catalog.schema.test",
         comment="Executes Python code and returns its stdout.",
     )
 
 
 @pytest.mark.asyncio
-async def test_uc_function_to_litellm_tool(uc_client):
-    """Test conversion of a UC function to a LiteLLM tool."""
+async def test_uc_function_to_autogen_tool(uc_client):
     mock_function_info = generate_function_info()
     with (
         mock.patch(
@@ -202,19 +238,11 @@ async def test_uc_function_to_litellm_tool(uc_client):
             return_value=FunctionExecutionResult(format="SCALAR", value="some_string"),
         ),
     ):
-        tool = UCFunctionToolkit.uc_function_to_litellm_tool(
+        tool = UCFunctionToolkit.uc_function_to_autogen_tool(
             function_name="catalog.schema.test", client=uc_client
         )
-<<<<<<< HEAD
-        assert tool.name == "catalog__schema__test"
-=======
         result = json.loads(tool.fn(x="some_string"))["value"]
         assert result == "some_string"
-
-        # Check defaults for parameters not defined by UC
-        assert tool.cache_function(1, {1: 1})
-        assert not tool.result_as_answer
-        assert not tool.description_updated
 
 
 @pytest.mark.asyncio
@@ -229,43 +257,41 @@ async def test_toolkit_with_invalid_function_input(uc_client):
         ),
         mock.patch.object(uc_client, "get_function", return_value=mock_function_info),
     ):
-        # Test with invalid input params that are not matching expected schema
         invalid_inputs = {"unexpected_key": "value"}
-        tool = UCFunctionToolkit.uc_function_to_litellm_tool(
+        tool = UCFunctionToolkit.uc_function_to_autogen_tool(
             function_name="catalog.schema.test", client=uc_client
         )
 
         with pytest.raises(ValueError, match="Extra parameters provided that are not defined"):
             tool.fn(**invalid_inputs)
->>>>>>> 44175c2 (OSS integration test)
 
 
-@pytest.mark.asyncio
-async def test_toolkit_litellm_kwarg_passthrough(uc_client):
-    """Test toolkit with keyword arguments for LitelLLM tool conversion."""
-    mock_function_info = generate_function_info()
+def test_register_with_agents(uc_client):
+    function_names = ["catalog.schema.function"]
 
-    with (
-        mock.patch(
-            "unitycatalog.ai.core.utils.client_utils.validate_or_set_default_client",
-            return_value=uc_client,
-        ),
-        mock.patch.object(uc_client, "get_function", return_value=mock_function_info),
+    FunctionInfo(
+        catalog_name="catalog",
+        schema_name="schema",
+        name="function",
+        input_params=FunctionParameterInfos(parameters=[]),
+        full_name="catalog.schema.function",
+        comment="Executes Python code and returns its stdout.",
+    )
+
+    mock_autogen_tool = mock.create_autospec(AutogenTool)
+    mock_autogen_tool.register_function = mock.MagicMock()
+
+    with mock.patch.object(
+        UCFunctionToolkit, "uc_function_to_autogen_tool", return_value=mock_autogen_tool
     ):
-        tool = UCFunctionToolkit.uc_function_to_litellm_tool(
-            function_name="catalog.schema.test",
-            client=uc_client,
-<<<<<<< HEAD
-        )
+        toolkit = UCFunctionToolkit(function_names=function_names, client=uc_client)
 
-        assert tool is not None
-=======
-            cache_function=lambda: True,
-            result_as_answer=True,
-            description_updated=True,
-        )
+        mock_callers = mock.MagicMock(spec=ConversableAgent)
+        mock_executors = mock.MagicMock(spec=ConversableAgent)
 
-        assert tool.cache_function()
-        assert tool.result_as_answer
-        assert tool.description_updated
->>>>>>> 44175c2 (OSS integration test)
+        toolkit.register_with_agents(callers=mock_callers, executors=mock_executors)
+
+        for tool in toolkit.tools:
+            tool.register_function.assert_called_once_with(
+                callers=mock_callers, executors=mock_executors
+            )
