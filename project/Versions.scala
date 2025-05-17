312f428d272e9a4b438fb6f2abf4b07f24b89a45
Jacek Laskowski <jacek@japila.pl>
2025-05-17 22:17:59 +0200

    Downgrade some dep versions
    Signed-off-by: Jacek Laskowski <jacek@japila.pl>

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,13 +1,13 @@
 object Versions {
   val antlrVersion = "4.9.3"
   val deltaVersion = "3.2.1"
-  val jacksonVersion = "2.19.0"
+  val jacksonVersion = "2.15.0"
   val log4jVersion = "2.24.3"
   val openApiToolsJacksonBindNullableVersion = "0.2.6"
   val orgApacheHttpVersion = "4.5.14"
   val scala212 = "2.12.15"
   val scala213 = "2.13.14"
-  val slf4jVersion = "2.0.17"
+  val slf4jVersion = "2.0.13"
   val spark3Version = "3.5.3"
   val spark4Version = "4.0.0"
 }
