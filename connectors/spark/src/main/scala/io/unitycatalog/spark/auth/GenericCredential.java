860e0f5bb00b4cdd08cf51d10e68f2737973f938
openinx <openinx@gmail.com>
2025-10-09 08:53:09 -0700

    Add AwsVendedTokenProvider to renew the correct credentials. (#1098)
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [x] If there is a related issue, make sure it is linked to this PR.
    - [x] If you've fixed a bug or added code that should be tested, add
    tests!
    - [x] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    Implement this aws token provider for this issue:
    https://github.com/unitycatalog/unitycatalog/issues/1096. This provider
    can accept the initialized credentials provider for the first time, and
    then it will automatically renew the credentials before the expiration
    time. So that the provider can always deliver the valid credentials to
    the upper layer.
    
    ---------
    
    Signed-off-by: Zheng Hu <openinx@gmail.com>

---
+++ connectors/spark/src/main/scala/io/unitycatalog/spark/auth/GenericCredential.java
@@ -0,0 +1,47 @@
+package io.unitycatalog.spark.auth;
+
+import io.unitycatalog.client.model.AwsCredentials;
+import io.unitycatalog.client.model.TemporaryCredentials;
+
+public class GenericCredential {
+  private final TemporaryCredentials tempCred;
+
+  public GenericCredential(TemporaryCredentials tempCred) {
+    this.tempCred = tempCred;
+  }
+
+  public static GenericCredential forAws(
+      String accessKey,
+      String secretKey,
+      String sessionToken,
+      long expiredTimeMillis) {
+    // Initialize the aws credentials.
+    AwsCredentials awsCredentials = new AwsCredentials();
+    awsCredentials.setAccessKeyId(accessKey);
+    awsCredentials.setSecretAccessKey(secretKey);
+    awsCredentials.setSessionToken(sessionToken);
+
+    // Initialize the unity catalog's temporary credentials.
+    TemporaryCredentials tempCred = new TemporaryCredentials();
+    tempCred.setAwsTempCredentials(awsCredentials);
+    tempCred.setExpirationTime(expiredTimeMillis);
+
+    return new GenericCredential(tempCred);
+  }
+
+  public TemporaryCredentials temporaryCredentials() {
+    return tempCred;
+  }
+
+  /**
+   * Decide whether it's time to renew the credential/token in advance.
+   *
+   * @param renewalLeadTimeMillis The amount of time before something expires when the renewal
+   *                              process should start.
+   * @return true if it's ready to renew.
+   */
+  public boolean readyToRenew(long renewalLeadTimeMillis) {
+    return tempCred.getExpirationTime() != null &&
+        tempCred.getExpirationTime() <= System.currentTimeMillis() + renewalLeadTimeMillis;
+  }
+}
