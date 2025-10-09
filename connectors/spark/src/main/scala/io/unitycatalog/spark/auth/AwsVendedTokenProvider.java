a730f33fd18491cebc85235f9108ac3e85d1de86
openinx <openinx@gmail.com>
2025-10-09 08:53:09 -0700

    Add AwsVendedTokenProvider to renew the correct credentials. (#1098)
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [x] If there is a related issue, make sure it is linked to this PR.
    - [x] If you've fixed a bug or added code that should be tested, add
    tests!
    - [x] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    Implement this aws token provider for this issue:
    https://github.com/unitycatalog/unitycatalog/issues/1096. This provider
    can accept the initialized credentials provider for the first time, and
    then it will automatically renew the credentials before the expiration
    time. So that the provider can always deliver the valid credentials to
    the upper layer.
    
    ---------
    
    Signed-off-by: Zheng Hu <openinx@gmail.com>

---
+++ connectors/spark/src/main/scala/io/unitycatalog/spark/auth/AwsVendedTokenProvider.java
@@ -0,0 +1,58 @@
+package io.unitycatalog.spark.auth;
+
+import io.unitycatalog.spark.UCHadoopConf;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.shaded.com.google.common.base.Preconditions;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+
+public class AwsVendedTokenProvider extends GenericCredentialProvider
+    implements AwsCredentialsProvider {
+
+  /**
+   * Constructor for the hadoop's CredentialProviderListFactory#buildAWSProviderList to initialize.
+   */
+  public AwsVendedTokenProvider(Configuration conf) {
+    super(conf);
+  }
+
+  @Override
+  public GenericCredential initGenericCredential(Configuration conf) {
+    if (conf.get(UCHadoopConf.S3A_INIT_ACCESS_KEY) != null
+        && conf.get(UCHadoopConf.S3A_INIT_SECRET_KEY) != null
+        && conf.get(UCHadoopConf.S3A_INIT_SESSION_TOKEN) != null
+        && conf.get(UCHadoopConf.S3A_INIT_CRED_EXPIRED_TIME) != null) {
+
+      String accessKey = conf.get(UCHadoopConf.S3A_INIT_ACCESS_KEY);
+      String secretKey = conf.get(UCHadoopConf.S3A_INIT_SECRET_KEY);
+      String sessionToken = conf.get(UCHadoopConf.S3A_INIT_SESSION_TOKEN);
+
+      long expiredTimeMillis = conf.getLong(UCHadoopConf.S3A_INIT_CRED_EXPIRED_TIME, 0L);
+      Preconditions.checkState(expiredTimeMillis > 0, "Expired time %s must be greater than 0, " +
+          "please check configure key '%s'", expiredTimeMillis, UCHadoopConf.S3A_INIT_CRED_EXPIRED_TIME);
+
+      return GenericCredential.forAws(accessKey, secretKey, sessionToken, expiredTimeMillis);
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public AwsCredentials resolveCredentials() {
+    GenericCredential generic = accessCredentials();
+
+    // Wrap the GenericCredential as an AwsCredentials.
+    io.unitycatalog.client.model.AwsCredentials awsTempCred = generic
+        .temporaryCredentials()
+        .getAwsTempCredentials();
+    Preconditions.checkNotNull(awsTempCred,
+        "AWS temp credential of generic credentials cannot be null");
+
+    return AwsSessionCredentials.builder()
+        .accessKeyId(awsTempCred.getAccessKeyId())
+        .secretAccessKey(awsTempCred.getSecretAccessKey())
+        .sessionToken(awsTempCred.getSessionToken())
+        .build();
+  }
+}
