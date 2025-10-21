efaa235be445e743e4f36d4aea966b52899c9150
Cyril Carrez <cyril.carrez@gmail.com>
2025-10-22 00:25:25 +0200

    Don't quote schema or table names when trying to load it from TablesAPI as it's not needed and will result in a 404.
    Signed-off-by: Cyril Carrez <cyril.carrez@gmail.com>

--- connectors/spark/src/main/scala/io/unitycatalog/spark/UCSingleCatalog.scala
+++ connectors/spark/src/main/scala/io/unitycatalog/spark/UCSingleCatalog.scala
@@ -217,7 +217,7 @@ private class UCProxy(
 
   override def loadTable(ident: Identifier): Table = {
     val t = try {
-      tablesApi.getTable(name + "." + ident.toString)
+      tablesApi.getTable(name + "." + ident.namespace().head + "." + ident.name())
     } catch {
       case e: ApiException if e.getCode == 404 =>
         throw new NoSuchTableException(ident)
