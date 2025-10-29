aac2e391566233dca76932d70aaf271f85ef88dc
Uros Stankovic <uros.stankovic@databricks.com>
2025-10-29 17:58:25 +0100

    Append UC base path to avoid breaking change.
    Signed-off-by: Uros Stankovic <uros.stankovic@databricks.com>

--- connectors/spark/src/main/scala/io/unitycatalog/spark/ApiClientFactory.java
+++ connectors/spark/src/main/scala/io/unitycatalog/spark/ApiClientFactory.java
@@ -5,14 +5,18 @@ import io.unitycatalog.client.ApiClient;
 import java.net.URI;
 
 public class ApiClientFactory {
+
+  public static final String BASE_PATH = "/api/2.1/unity-catalog";
+
   private ApiClientFactory() {}
 
   public static ApiClient createApiClient(URI url, String token) {
+    String basePath = url.getPath() + BASE_PATH;
     ApiClient apiClient = new ApiClient()
         .setHost(url.getHost())
         .setPort(url.getPort())
         .setScheme(url.getScheme())
-        .setBasePath(url.getPath());
+        .setBasePath(basePath);
 
     if (token != null && !token.isEmpty()) {
       apiClient = apiClient.setRequestInterceptor(
