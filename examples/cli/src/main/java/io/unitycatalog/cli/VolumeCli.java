72ddfe72e65d66112c51e18dae0054301e9f2672
Yi Li <yi.li@databricks.com>
2025-11-11 16:50:05 -0800

    Add CLI support for MANAGED tables with CLI tests (#1136)
    ## 🥞 Stacked PR
    Use this
    [link](https://github.com/unitycatalog/unitycatalog/pull/1136/files) to
    review incremental changes.
    -
    [**staging_table_cli_test**](https://github.com/unitycatalog/unitycatalog/pull/1136)
    [[Files
    changed](https://github.com/unitycatalog/unitycatalog/pull/1136/files)]
    
    ---------
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [x] If there is a related issue, make sure it is linked to this PR.
    - [ ] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
        Add CLI support for MANAGED tables and comprehensive tests
    
        - Enhance TableCli with MANAGED table creation
    - Update DeltaKernelUtils with staging table support and writer
    capabilities
        - Add comprehensive test coverage for managed and staging tables
    <!-- Please state what you've changed and how it might affect the users.
    -->
    Related issue is:
    https://github.com/unitycatalog/unitycatalog/issues/1143
    
    ---------
    
    Signed-off-by: Yi Li <yi.li@databricks.com>

--- examples/cli/src/main/java/io/unitycatalog/cli/VolumeCli.java
+++ examples/cli/src/main/java/io/unitycatalog/cli/VolumeCli.java
@@ -117,12 +117,10 @@ public class VolumeCli {
     Configuration conf =
         DeltaKernelUtils.getHDFSConfiguration(
             baseURI,
-            tempCredApi
-                .generateTemporaryVolumeCredentials(
-                    new GenerateTemporaryVolumeCredential()
-                        .volumeId(volumeInfo.getVolumeId())
-                        .operation(VolumeOperation.WRITE_VOLUME))
-                .getAwsTempCredentials());
+            tempCredApi.generateTemporaryVolumeCredentials(
+                new GenerateTemporaryVolumeCredential()
+                    .volumeId(volumeInfo.getVolumeId())
+                    .operation(VolumeOperation.WRITE_VOLUME)));
     FileSystem fs =
         DeltaKernelUtils.getFileSystem(
             URI.create(DeltaKernelUtils.substituteSchemeForS3(volumeLocation)), conf);
@@ -178,12 +176,10 @@ public class VolumeCli {
     Configuration conf =
         DeltaKernelUtils.getHDFSConfiguration(
             relativeURI,
-            tempCredApi
-                .generateTemporaryVolumeCredentials(
-                    new GenerateTemporaryVolumeCredential()
-                        .volumeId(volumeInfo.getVolumeId())
-                        .operation(VolumeOperation.READ_VOLUME))
-                .getAwsTempCredentials());
+            tempCredApi.generateTemporaryVolumeCredentials(
+                new GenerateTemporaryVolumeCredential()
+                    .volumeId(volumeInfo.getVolumeId())
+                    .operation(VolumeOperation.READ_VOLUME)));
     URI relativeURIWithS3AScheme =
         URI.create(DeltaKernelUtils.substituteSchemeForS3(relativeURI.toString()));
     FileSystem fs =
