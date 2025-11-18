053d6b2f025b32854d089c48bba7a0c0cfcb4923
openinx <openinx@gmail.com>
2025-11-18 08:56:55 -0800

    [0.3] Add local Abfs credential renewal integration test. (#1213)
    **PR Checklist**
    
    - [x] A description of the changes is added to the description of this
    PR.
    - [x] If there is a related issue, make sure it is linked to this PR.
    - [x] If you've fixed a bug or added code that should be tested, add
    tests!
    - [ ] If you've added or modified a feature, documentation in `docs` is
    updated
    
    **Description of changes**
    
    Backport the [PR
    change](https://github.com/unitycatalog/unitycatalog/pull/1193) of
    [issue](https://github.com/unitycatalog/unitycatalog/issues/1175) to
    branch-0.3
    The backport progress is here:
    https://github.com/unitycatalog/unitycatalog/issues/1163
    
    
    After merged this
    https://github.com/unitycatalog/unitycatalog/pull/1150, there is no
    conflict any more. So it's a clean patch.
    
    Signed-off-by: Zheng Hu <openinx@gmail.com>

--- connectors/spark/src/test/java/io/unitycatalog/spark/auth/BaseCredRenewITTest.java
+++ connectors/spark/src/test/java/io/unitycatalog/spark/auth/BaseCredRenewITTest.java
@@ -12,7 +12,6 @@ import io.unitycatalog.server.base.catalog.CatalogOperations;
 import io.unitycatalog.server.sdk.catalog.SdkCatalogOperations;
 import io.unitycatalog.server.sdk.schema.SdkSchemaOperations;
 import io.unitycatalog.server.service.credential.CredentialContext;
-import io.unitycatalog.server.service.credential.aws.CredentialsGenerator;
 import io.unitycatalog.spark.CredentialTestFileSystem;
 import io.unitycatalog.spark.UCHadoopConf;
 import io.unitycatalog.spark.UCSingleCatalog;
@@ -20,11 +19,12 @@ import io.unitycatalog.spark.utils.Clock;
 import java.io.File;
 import java.net.URI;
 import java.time.Duration;
-import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -42,33 +42,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.sparkproject.guava.collect.ImmutableList;
 import org.sparkproject.guava.collect.Iterators;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
-import software.amazon.awssdk.services.sts.model.Credentials;
 
 /**
- * Integration test to verify that AWS credential renewal works as expected.
+ * Integration test to verify that cloud vendor credential renewal works as expected.
  *
  * <p>This test sets up the Unity Catalog server and a local Spark cluster, then runs a custom Spark
  * job to validate credential renewal.
  *
- * <p>The approach is as follows: a testing {@link AwsCredentialsProvider} is injected into the
- * local Unity Catalog server, issuing a new credential every 30-second interval. The Spark job uses
- * {@link S3CredFileSystem} to check that the credential matches the current time window for each
- * filesystem access. {@link S3CredFileSystem} also tracks the number of renewals, allowing
+ * <p>The approach is as follows: a testing credential generator is injected into the local Unity
+ * Catalog server, issuing a new credential every 30-second interval. The Spark job uses {@link
+ * CredRenewFileSystem} to check that the credential matches the current time window for each
+ * filesystem access. {@link CredRenewFileSystem} also tracks the number of renewals, allowing
  * verification that credential renewal occurs as expected.
  */
-public class AwsCredentialRenewalTest extends BaseCRUDTest {
-
-  private static final String CLOCK_NAME = AwsCredentialRenewalTest.class.getSimpleName();
-  private static final String CREDENTIALS_GENERATOR = TimeBasedCredentialsGenerator.class.getName();
-
+public abstract class BaseCredRenewITTest extends BaseCRUDTest {
+  private static final String CLOCK_NAME = UUID.randomUUID().toString();
   private static final String CATALOG_NAME = "CredRenewalCatalog";
   private static final String SCHEMA_NAME = "Default";
   private static final String TABLE_NAME = String.format("%s.%s.demo", CATALOG_NAME, SCHEMA_NAME);
-  private static final String BUCKET_NAME = "test-bucket";
-  private static final String S3_SCHEME = "s3:";
-  private static final long DEFAULT_INTERVAL_MILLIS = 30_000L;
+  protected static final String BUCKET_NAME = "test-bucket";
+  protected static final long DEFAULT_INTERVAL_MILLIS = 30_000L;
 
   @TempDir private File dataDir;
   private SparkSession session;
@@ -79,41 +72,39 @@ public class AwsCredentialRenewalTest extends BaseCRUDTest {
     return new SdkCatalogOperations(createApiClient(config));
   }
 
-  @Override
-  protected void setUpProperties() {
-    super.setUpProperties();
-    serverProperties.put("s3.bucketPath.0", "s3://" + BUCKET_NAME);
-    serverProperties.put("s3.accessKey.0", "accessKey0");
-    serverProperties.put("s3.secretKey.0", "secretKey0");
-    serverProperties.put("s3.sessionToken.0", "sessionToken0");
-    // Customize the test credential generator to issue a new credential every 30-second interval.
-    // This allows us to verify whether credential renewal is functioning correctly by checking
-    // if the current credential matches the expected time window.
-    serverProperties.put("s3.credentialsGenerator.0", CREDENTIALS_GENERATOR);
-  }
+  protected abstract String scheme();
+
+  protected abstract Map<String, String> catalogExtraProps();
 
   private SparkSession createSparkSession() {
+    SparkSession.Builder builder =
+        SparkSession.builder()
+            .appName("test-cloud-vendor-credential-renewal")
+            .master("local[1]") // Make it single-threaded explicitly.
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+            .config(
+                "spark.sql.catalog.spark_catalog",
+                "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+            .config("spark.hadoop." + UCHadoopConf.UC_TEST_CLOCK_NAME, CLOCK_NAME)
+            .config("spark.hadoop." + UCHadoopConf.UC_RENEWAL_LEAD_TIME_KEY, 0L)
+            .config("spark.sql.shuffle.partitions", "1");
+
+    // Set the default catalog properties.
     String testCatalogKey = String.format("spark.sql.catalog.%s", CATALOG_NAME);
-    return SparkSession.builder()
-        .appName("test-credential-renewal")
-        .master("local[1]") // Make it single-threaded explicitly.
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-        .config(
-            "spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-        .config("spark.hadoop." + UCHadoopConf.UC_TEST_CLOCK_NAME, CLOCK_NAME)
-        .config("spark.hadoop." + UCHadoopConf.UC_RENEWAL_LEAD_TIME_KEY, 0L)
-        .config("spark.sql.shuffle.partitions", "1")
+    builder
         .config(testCatalogKey, UCSingleCatalog.class.getName())
         .config(testCatalogKey + ".uri", serverConfig.getServerUrl())
         .config(testCatalogKey + ".token", serverConfig.getAuthToken())
         .config(testCatalogKey + ".warehouse", CATALOG_NAME)
-        .config(testCatalogKey + ".renewCredential.enabled", "true")
-        .config("fs.s3.impl", S3CredFileSystem.class.getName())
-        .getOrCreate();
+        .config(testCatalogKey + ".renewCredential.enabled", "true");
+
+    // Set the customized catalog properties.
+    catalogExtraProps().forEach(builder::config);
+
+    return builder.getOrCreate();
   }
 
   private interface Callable {
-
     void call() throws Exception;
   }
 
@@ -165,9 +156,13 @@ public class AwsCredentialRenewalTest extends BaseCRUDTest {
     return Clock.getManualClock(CLOCK_NAME);
   }
 
+  private String bucketRoot() {
+    return String.format("%s://%s", scheme(), BUCKET_NAME);
+  }
+
   @Test
   public void testFileSystemRenewal() throws Exception {
-    String location = String.format("s3://%s%s/fs", BUCKET_NAME, dataDir.getCanonicalPath());
+    String location = String.format("%s%s/fs", bucketRoot(), dataDir.getCanonicalPath());
     Path locPath = new Path(location);
 
     // Create the external delta table in catalog.
@@ -204,7 +199,8 @@ public class AwsCredentialRenewalTest extends BaseCRUDTest {
             .map(
                 row -> {
                   Configuration conf = serialConf.value();
-                  S3CredFileSystem fs = (S3CredFileSystem) FileSystem.get(new URI(location), conf);
+                  CredRenewFileSystem<?> fs =
+                      (CredRenewFileSystem<?>) FileSystem.get(new URI(location), conf);
 
                   for (int refreshIndex = 0; refreshIndex < 10; refreshIndex += 1) {
                     // Pre-check before the credential renewal.
@@ -229,8 +225,8 @@ public class AwsCredentialRenewalTest extends BaseCRUDTest {
 
   @Test
   public void testDeltaReadWriteRenewal() throws Exception {
-    String srcLoc = String.format("s3://%s%s/src", BUCKET_NAME, dataDir.getCanonicalPath());
-    String dstLoc = String.format("s3://%s%s/dst", BUCKET_NAME, dataDir.getCanonicalPath());
+    String srcLoc = String.format("%s%s/src", bucketRoot(), dataDir.getCanonicalPath());
+    String dstLoc = String.format("%s%s/dst", bucketRoot(), dataDir.getCanonicalPath());
     String srcTable = String.format("%s.%s.src", CATALOG_NAME, SCHEMA_NAME);
     String dstTable = String.format("%s.%s.dst", CATALOG_NAME, SCHEMA_NAME);
 
@@ -278,81 +274,62 @@ public class AwsCredentialRenewalTest extends BaseCRUDTest {
   }
 
   /**
-   * A customized {@link CredentialsGenerator} that generates credentials based on time intervals.
-   * The entire timeline is divided into consecutive 30-second windows, and all requests that fall
-   * within the same window will receive the same credential. This generator is dynamically loaded
-   * by the Unity Catalog server and serves credential generation requests from client REST API
-   * calls.
+   * A customized credential provider that generates credentials based on time intervals. The entire
+   * timeline is divided into consecutive 30-second windows, and all requests that fall within the
+   * same window will receive the same credential. This generator is dynamically loaded by the Unity
+   * Catalog server and serves credential generation requests from client REST API calls.
    */
-  public static class TimeBasedCredentialsGenerator implements CredentialsGenerator {
-
-    @Override
-    public Credentials generate(CredentialContext credentialContext) {
+  public abstract static class TimeBasedCredGenerator<T> {
+    public T generate(CredentialContext ignored) {
       long curTsMillis = testClock().now().toEpochMilli();
       // Align it into the window [starTs, starTs + DEFAULT_INTERVAL_MILLIS].
       long startTsMillis = curTsMillis / DEFAULT_INTERVAL_MILLIS * DEFAULT_INTERVAL_MILLIS;
-      return Credentials.builder()
-          .accessKeyId("accessKeyId" + startTsMillis)
-          .secretAccessKey("secretAccessKey" + startTsMillis)
-          .sessionToken("sessionToken" + startTsMillis)
-          .expiration(Instant.ofEpochMilli(startTsMillis + DEFAULT_INTERVAL_MILLIS))
-          .build();
+      return newTimeBasedCred(startTsMillis);
     }
+
+    protected abstract T newTimeBasedCred(long ts);
   }
 
   /**
-   * A testing S3 filesystem used to verify credential renewal behavior. For each {@code
+   * A testing filesystem used to verify credential renewal behavior. For each {@code
    * checkCredentials()} call, the previous credentials should automatically renew as the 30-second
    * time window advances. The test tracks how many distinct credentials this filesystem receives,
    * which should match the expected number of credential renewals. We use this filesystem to
    * accurately track how many renewal happened.
    */
-  public static class S3CredFileSystem extends CredentialTestFileSystem {
-
+  public abstract static class CredRenewFileSystem<T> extends CredentialTestFileSystem {
     private final Set<Long> verifiedTs = new HashSet<>();
-    private volatile AwsCredentialsProvider lazyProvider;
+    private volatile T lazyProvider;
 
     @Override
     protected void checkCredentials(Path f) {
       String host = f.toUri().getHost();
 
       if (credentialCheckEnabled && BUCKET_NAME.equals(host)) {
-        AwsCredentialsProvider provider = accessProvider();
+        T provider = accessProvider();
         assertThat(provider).isNotNull();
 
-        AwsSessionCredentials cred = (AwsSessionCredentials) provider.resolveCredentials();
-        long ts =
-            testClock().now().toEpochMilli() / DEFAULT_INTERVAL_MILLIS * DEFAULT_INTERVAL_MILLIS;
-        assertThat(cred.accessKeyId()).isEqualTo("accessKeyId" + ts);
-        assertThat(cred.secretAccessKey()).isEqualTo("secretAccessKey" + ts);
-        assertThat(cred.sessionToken()).isEqualTo("sessionToken" + ts);
+        long curTs = testClock().now().toEpochMilli();
+        long windowStartTs = curTs / DEFAULT_INTERVAL_MILLIS * DEFAULT_INTERVAL_MILLIS;
+        assertCredentials(provider, windowStartTs);
 
-        verifiedTs.add(ts);
+        verifiedTs.add(windowStartTs);
       }
     }
+
+    private synchronized T accessProvider() {
+      if (lazyProvider == null) {
+        lazyProvider = createProvider();
+      }
+      return lazyProvider;
+    }
+
+    protected abstract T createProvider();
+
+    protected abstract void assertCredentials(T provider, long ts);
 
     public int renewalCount() {
       return verifiedTs.size() - 1;
-    }
-
-    @Override
-    protected String scheme() {
-      return S3_SCHEME;
-    }
-
-    private synchronized AwsCredentialsProvider accessProvider() {
-      if (lazyProvider != null) {
-        return lazyProvider;
-      }
-
-      String clazz = getConf().get(UCHadoopConf.S3A_CREDENTIALS_PROVIDER);
-      assertThat(clazz).isEqualTo(AwsVendedTokenProvider.class.getName());
-
-      // This will validate if the hadoop configuration is correct or not, since it will fail the
-      // provider constructor if given an incorrect setting here.
-      lazyProvider = new AwsVendedTokenProvider(getConf());
-
-      return lazyProvider;
     }
   }
 }
