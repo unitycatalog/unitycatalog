e3f00012b43818cb19f96f3b5eb4de958e35b5ec
Zheng Hu <openinx@gmail.com>
2025-11-22 21:55:25 -0800

    Add OAuth real production integration test.
    Signed-off-by: Zheng Hu <openinx@gmail.com>

--- integration-tests/src/test/java/io/unitycatalog/integrationtests/TestUtils.java
+++ integration-tests/src/test/java/io/unitycatalog/integrationtests/TestUtils.java
@@ -1,15 +1,27 @@
 package io.unitycatalog.integrationtests;
 
+import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class TestUtils {
-  public static final String SERVER_URL = envAsString("CATALOG_URI", "http://localhost:8080");
-  public static final String AUTH_TOKEN = envAsString("CATALOG_AUTH_TOKEN", "");
   public static final String CATALOG_NAME = envAsString("CATALOG_NAME", "unity");
-  public static final String S3_BASE_LOCATION = envAsString("S3_BASE_LOCATION", "s3://bucket/key");
-  public static final String GS_BASE_LOCATION = envAsString("GS_BASE_LOCATION", "gs://bucket/key");
-  public static final String ABFSS_BASE_LOCATION =
-      envAsString("ABFSS_BASE_LOCATION", "abfss://bucket/key");
+  public static final String SCHEMA_NAME = envAsString("SCHEMA_NAME", "UcIntegrationTest");
+  public static final String SERVER_URL = envAsString("CATALOG_URI", "http://localhost:8080");
+
+  // Fixed token to authenticate unity catalog server.
+  public static final String AUTH_TOKEN = envAsString("CATALOG_AUTH_TOKEN", "");
+
+  // OAuth to authenticate unity catalog server.
+  public static final String OAUTH_URI = envAsString("CATALOG_OAUTH_URI", "");
+  public static final String OAUTH_CLIENT_ID = envAsString("CATALOG_OAUTH_CLIENT_ID", "");
+  public static final String OAUTH_CLIENT_SECRET = envAsString("CATALOG_OAUTH_CLIENT_SECRET", "");
+
+  // Base locations.
+  public static final String S3_BASE_LOCATION = envAsString("S3_BASE_LOCATION", "");
+  public static final String GS_BASE_LOCATION = envAsString("GS_BASE_LOCATION", "");
+  public static final String ABFSS_BASE_LOCATION = envAsString("ABFSS_BASE_LOCATION", "");
+  public static final List<String> BASE_LOCATIONS =
+      List.of(S3_BASE_LOCATION, GS_BASE_LOCATION, ABFSS_BASE_LOCATION);
 
   public static String envAsString(String key, String defaultValue) {
     return System.getenv().getOrDefault(key, defaultValue);
@@ -27,5 +39,9 @@ public class TestUtils {
 
   public static String randomName() {
     return RandomStringUtils.secure().nextAlphabetic(8);
+  }
+
+  public static boolean isEmptyOrNull(String value) {
+    return value == null || value.isEmpty();
   }
 }
