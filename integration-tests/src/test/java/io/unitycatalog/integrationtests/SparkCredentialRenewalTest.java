d8586f3c737852a70638175c7effd78386159b2e
Zheng Hu <openinx@gmail.com>
2025-11-22 21:55:25 -0800

    Add OAuth real production integration test.
    Signed-off-by: Zheng Hu <openinx@gmail.com>

--- integration-tests/src/test/java/io/unitycatalog/integrationtests/SparkCredentialRenewalTest.java
+++ integration-tests/src/test/java/io/unitycatalog/integrationtests/SparkCredentialRenewalTest.java
@@ -1,24 +1,32 @@
 package io.unitycatalog.integrationtests;
 
 import static io.unitycatalog.integrationtests.TestUtils.AUTH_TOKEN;
+import static io.unitycatalog.integrationtests.TestUtils.BASE_LOCATIONS;
 import static io.unitycatalog.integrationtests.TestUtils.CATALOG_NAME;
-import static io.unitycatalog.integrationtests.TestUtils.S3_BASE_LOCATION;
+import static io.unitycatalog.integrationtests.TestUtils.OAUTH_CLIENT_ID;
+import static io.unitycatalog.integrationtests.TestUtils.OAUTH_CLIENT_SECRET;
+import static io.unitycatalog.integrationtests.TestUtils.OAUTH_URI;
+import static io.unitycatalog.integrationtests.TestUtils.SCHEMA_NAME;
 import static io.unitycatalog.integrationtests.TestUtils.SERVER_URL;
 import static io.unitycatalog.integrationtests.TestUtils.envAsBoolean;
 import static io.unitycatalog.integrationtests.TestUtils.envAsLong;
-import static io.unitycatalog.integrationtests.TestUtils.randomName;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import io.unitycatalog.spark.UCSingleCatalog;
+import io.unitycatalog.spark.utils.OptionsUtil;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Stream;
 import org.apache.spark.api.java.function.MapPartitionsFunction;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.sparkproject.guava.collect.Iterators;
 
 /**
@@ -34,9 +42,10 @@ import org.sparkproject.guava.collect.Iterators;
  * <p><b>To run this tests: </b>
  *
  * <pre>
+ * export CATALOG_NAME=...
+ * export SCHEMA_NAME=...
  * export CATALOG_URI=...
  * export CATALOG_AUTH_TOKEN=...
- * export CATALOG_NAME=...
  * export S3_BASE_LOCATION=...
  * SBT_OPTS="-Xmx8G -XX:+UseG1GC" \
  * ./build/sbt \
@@ -51,21 +60,38 @@ public class SparkCredentialRenewalTest {
 
   // Define the CREDENTIAL_RENEWAL_TEST_DURATION_SECONDS environment variable, which control how
   // long will the test run. 90 minutes by default.
-  private static final long DURATION_SECONDS = envAsLong(PREFIX + "DURATION_SECONDS", 5600L);
+  private static final long DURATION_SECONDS = envAsLong(PREFIX + "DURATION_SECONDS", 5400L);
 
-  // Define the schema name and table name.
-  private static final String SCHEMA_NAME = randomName();
-  private static final String SRC_TABLE =
-      String.format("%s.%s.src_%s", CATALOG_NAME, SCHEMA_NAME, randomName());
-  private static final String DST_TABLE =
-      String.format("%s.%s.dst_%s", CATALOG_NAME, SCHEMA_NAME, randomName());
+  // Define the CREDENTIAL_RENEWAL_TEST_MANAGED_TABLE_ENABLED environment variable, which decides
+  // if enable the managed table or not.
+  private static final boolean MANAGED_TABLE_ENABLED =
+      envAsBoolean(PREFIX + "MANAGED_TABLE_ENABLED", true);
+
+  // Define managed table name.
+  private static final String MANAGED_SRC_TABLE =
+      String.format("%s.%s.managedSrcCredRenewal", CATALOG_NAME, SCHEMA_NAME);
+  private static final String MANAGED_DST_TABLE =
+      String.format("%s.%s.managedDstCredRenewal", CATALOG_NAME, SCHEMA_NAME);
+
+  // Define the external table name.
+  private static final String EXTERNAL_SRC_TABLE =
+      String.format("%s.%s.externalSrcCredRenewal", CATALOG_NAME, SCHEMA_NAME);
+  private static final String EXTERNAL_DST_TABLE =
+      String.format("%s.%s.externalDstCredRenewal", CATALOG_NAME, SCHEMA_NAME);
 
   private static SparkSession spark;
 
-  @BeforeAll
-  public static void beforeAll() {
-    String testCatalogKey = String.format("spark.sql.catalog.%s", CATALOG_NAME);
-    spark =
+  private static String srcTable(boolean managedTable) {
+    return managedTable ? MANAGED_SRC_TABLE : EXTERNAL_SRC_TABLE;
+  }
+
+  private static String dstTable(boolean managedTable) {
+    return managedTable ? MANAGED_DST_TABLE : EXTERNAL_DST_TABLE;
+  }
+
+  private void setupSparkAndTable(
+      boolean managedTable, String baseLocation, Map<String, String> catalogProps) {
+    SparkSession.Builder builder =
         SparkSession.builder()
             .appName("test-credential-renewal-in-long-running-job")
             .master("local[1]") // Make it single-threaded explicitly.
@@ -74,37 +100,59 @@ public class SparkCredentialRenewalTest {
             .config(
                 "spark.sql.catalog.spark_catalog",
                 "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-            .config(testCatalogKey, UCSingleCatalog.class.getName())
-            .config(testCatalogKey + ".uri", SERVER_URL)
-            .config(testCatalogKey + ".token", AUTH_TOKEN)
-            .config(testCatalogKey + ".warehouse", CATALOG_NAME)
-            .config(testCatalogKey + ".renewCredential.enabled", String.valueOf(RENEW_CRED_ENABLED))
-            .getOrCreate();
+            .config("spark.sql.adaptive.enabled", "false")
+            .config("spark.sql.adaptive.coalescePartitions.enabled", "false");
 
-    sql("CREATE SCHEMA %s.%s", CATALOG_NAME, SCHEMA_NAME);
-    sql(
-        "CREATE TABLE %s (id INT) USING delta LOCATION '%s/%s' PARTITIONED BY (partition INT)",
-        SRC_TABLE, S3_BASE_LOCATION, UUID.randomUUID());
-    sql(
-        "CREATE TABLE %s (id INT) USING delta LOCATION '%s/%s'",
-        DST_TABLE, S3_BASE_LOCATION, UUID.randomUUID());
+    String testCatalogKey = String.format("spark.sql.catalog.%s", CATALOG_NAME);
+    // Use the UCSingleCatalog by default.
+    builder.config(testCatalogKey, UCSingleCatalog.class.getName());
+    // Set all the specified catalog props.
+    catalogProps.forEach((k, v) -> builder.config(testCatalogKey + "." + k, v));
+    spark = builder.getOrCreate();
+
+    sql("CREATE SCHEMA IF NOT EXISTS %s.%s", CATALOG_NAME, SCHEMA_NAME);
+    if (managedTable) {
+      // TODO: SQL-managed table creation is not supported yet, so we create the table manually.
+      // Once SQL support is added, use the SQL to create tables.
+      sql("DELETE FROM %s", srcTable(true));
+      sql("DELETE FROM %s", dstTable(true));
+    } else {
+      sql("DROP TABLE IF EXISTS %s", srcTable(false));
+      sql("DROP TABLE IF EXISTS %s", dstTable(false));
+      sql(
+          "CREATE TABLE %s (id INT) USING delta LOCATION '%s/%s' PARTITIONED BY (partition INT)",
+          srcTable(false), baseLocation, UUID.randomUUID());
+      sql(
+          "CREATE TABLE %s (id INT) USING delta LOCATION '%s/%s'",
+          dstTable(false), baseLocation, UUID.randomUUID());
+    }
   }
 
-  @AfterAll
-  public static void afterAll() {
-    sql("DROP TABLE IF EXISTS %s", SRC_TABLE);
-    sql("DROP TABLE IF EXISTS %s", DST_TABLE);
-    sql("DROP SCHEMA IF EXISTS %s.%s", CATALOG_NAME, SCHEMA_NAME);
-    spark.stop();
+  @AfterEach
+  public void afterEach() {
+    sql("DROP TABLE IF EXISTS %s", srcTable(false));
+    sql("DROP TABLE IF EXISTS %s", dstTable(false));
+
+    if (spark != null) {
+      spark.stop();
+      spark = null;
+    }
   }
 
-  @Test
-  public void testLongRunningJob() {
+  @ParameterizedTest
+  @MethodSource("credentialRenewalArguments")
+  public void testLongRunningJob(
+      boolean managedTable, String baseLocation, Map<String, String> catalogProps) {
+    // Prepare the spark setup, and both source table and destination table creation.
+    setupSparkAndTable(managedTable, baseLocation, catalogProps);
+    String srcTable = srcTable(managedTable);
+    String dstTable = dstTable(managedTable);
+
     // Every 100 seconds produce a row, because we don't want to produce massive small files.
     long rowCount = DURATION_SECONDS / 100 + (DURATION_SECONDS % 100 == 0 ? 0 : 1);
 
     // Generate data for the Delta source table.
-    sql("INSERT INTO %s SELECT id, id FROM range(0, %s)", SRC_TABLE, rowCount);
+    sql("INSERT INTO %s SELECT id, id FROM range(0, %s)", srcTable, rowCount);
 
     // Read from the Delta source table, mapping each partition to a separate task, and finally
     // write back to the destination table. With parallelism set to 1, tasks for each partition
@@ -115,7 +163,8 @@ public class SparkCredentialRenewalTest {
     spark
         .read()
         .format("delta")
-        .table(SRC_TABLE)
+        .table(srcTable)
+        .repartition((int) rowCount)
         .mapPartitions(
             (MapPartitionsFunction<Row, Integer>)
                 input -> {
@@ -127,14 +176,71 @@ public class SparkCredentialRenewalTest {
         .write()
         .format("delta")
         .mode("append")
-        .saveAsTable(DST_TABLE);
+        .saveAsTable(dstTable);
 
-    List<Row> results = sql("SELECT COUNT(*) FROM %s", DST_TABLE);
+    List<Row> results = sql("SELECT COUNT(*) FROM %s", dstTable);
     assertThat(results.size()).isEqualTo(1);
     assertThat(results.get(0).getLong(0)).isEqualTo(rowCount);
   }
 
   private static List<Row> sql(String statement, Object... args) {
     return spark.sql(String.format(statement, args)).collectAsList();
+  }
+
+  /**
+   * Generates argument combinations for long-running tests. Each {@link Arguments} instance
+   * contains:
+   *
+   * <ul>
+   *   <li><b>managedTable</b> - true indicate covering the managed table long-running test,
+   *       otherwise the external table long-running test.
+   *   <li><b>baseLocation</b> – The base cloud storage path used during testing. External tables
+   *       created in the tests will be placed under this location.
+   *   <li><b>catalogProps</b> – A {@code Map<String, String>} of catalog configuration properties.
+   *       The generated combinations cover both authentication modes: Personal Access Token (PAT)
+   *       and OAuth.
+   * </ul>
+   *
+   * @return A stream or collection of argument combinations used for parameterized tests.
+   */
+  public static Stream<Arguments> credentialRenewalArguments() {
+    List<Arguments> testArguments = new ArrayList<>();
+
+    List<Boolean> managedTableFlags = MANAGED_TABLE_ENABLED ? List.of(false, true) : List.of(false);
+    for (boolean managedTable : managedTableFlags) {
+      for (String baseLocation : BASE_LOCATIONS) {
+        if (!TestUtils.isEmptyOrNull(baseLocation)) {
+          // Add the Personal Access Token (PAT) test case if specified in the env vars.
+          if (!TestUtils.isEmptyOrNull(AUTH_TOKEN)) {
+            testArguments.add(
+                Arguments.of(
+                    managedTable,
+                    baseLocation,
+                    Map.of(
+                        OptionsUtil.URI, SERVER_URL,
+                        OptionsUtil.TOKEN, AUTH_TOKEN,
+                        OptionsUtil.WAREHOUSE, CATALOG_NAME,
+                        OptionsUtil.RENEW_CREDENTIAL_ENABLED, String.valueOf(RENEW_CRED_ENABLED))));
+          }
+
+          // Add the OAuth test case if specified in env vars.
+          if (!TestUtils.isEmptyOrNull(OAUTH_URI)) {
+            testArguments.add(
+                Arguments.of(
+                    managedTable,
+                    baseLocation,
+                    Map.of(
+                        OptionsUtil.URI, SERVER_URL,
+                        OptionsUtil.OAUTH_URI, OAUTH_URI,
+                        OptionsUtil.OAUTH_CLIENT_ID, OAUTH_CLIENT_ID,
+                        OptionsUtil.OAUTH_CLIENT_SECRET, OAUTH_CLIENT_SECRET,
+                        OptionsUtil.WAREHOUSE, CATALOG_NAME,
+                        OptionsUtil.RENEW_CREDENTIAL_ENABLED, String.valueOf(RENEW_CRED_ENABLED))));
+          }
+        }
+      }
+    }
+
+    return testArguments.stream();
   }
 }
