b354a834db68b6fc697248b716f492162749e0e3
1b5d <8110504+1b5d@users.noreply.github.com>
2025-11-24 16:39:09 +0100

    refactor test
    Signed-off-by: 1b5d <8110504+1b5d@users.noreply.github.com>

--- server/src/test/java/io/unitycatalog/server/service/TemporaryPathCredentialsServiceTest.java
+++ server/src/test/java/io/unitycatalog/server/service/TemporaryPathCredentialsServiceTest.java
@@ -1,10 +1,12 @@
 package io.unitycatalog.server.service;
 
-import static io.unitycatalog.server.model.SecurableType.EXTERNAL_LOCATION;
-import static io.unitycatalog.server.security.SecurityContext.Issuers.INTERNAL;
+import static io.unitycatalog.client.model.SecurableType.EXTERNAL_LOCATION;
+import static io.unitycatalog.server.persist.model.Privileges.EXTERNAL_USE_LOCATION;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
-import com.auth0.jwt.JWT;
 import com.linecorp.armeria.client.WebClient;
 import com.linecorp.armeria.common.AggregatedHttpResponse;
 import com.linecorp.armeria.common.HttpMethod;
@@ -12,45 +14,33 @@ import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.common.MediaType;
 import com.linecorp.armeria.common.RequestHeaders;
 import com.linecorp.armeria.common.auth.AuthToken;
-import io.unitycatalog.client.ApiException;
 import io.unitycatalog.client.model.AwsIamRoleRequest;
 import io.unitycatalog.client.model.CreateCredentialRequest;
 import io.unitycatalog.client.model.CreateExternalLocation;
 import io.unitycatalog.client.model.CredentialPurpose;
 import io.unitycatalog.client.model.PathOperation;
-import io.unitycatalog.control.model.User;
-import io.unitycatalog.server.base.BaseCRUDTestWithMockCredentials;
 import io.unitycatalog.server.base.ServerConfig;
-import io.unitycatalog.server.base.catalog.CatalogOperations;
 import io.unitycatalog.server.base.credential.CredentialOperations;
 import io.unitycatalog.server.base.externallocation.ExternalLocationOperations;
-import io.unitycatalog.server.exception.BaseException;
-import io.unitycatalog.server.model.SecurableType;
-import io.unitycatalog.server.persist.Repositories;
-import io.unitycatalog.server.persist.model.CreateUser;
-import io.unitycatalog.server.persist.model.Privileges;
+import io.unitycatalog.server.sdk.access.SdkAccessControlBaseCRUDTest;
 import io.unitycatalog.server.sdk.externallocation.SdkExternalLocationOperations;
 import io.unitycatalog.server.sdk.storagecredential.SdkCredentialOperations;
-import io.unitycatalog.server.security.JwtClaim;
-import io.unitycatalog.server.security.JwtTokenType;
-import io.unitycatalog.server.security.SecurityConfiguration;
-import io.unitycatalog.server.security.SecurityContext;
+import io.unitycatalog.server.service.credential.CloudCredentialVendor;
+import io.unitycatalog.server.service.credential.CredentialContext;
+import io.unitycatalog.server.service.credential.aws.AwsCredentialVendor;
+import io.unitycatalog.server.utils.ServerProperties;
 import io.unitycatalog.server.utils.TestUtils;
-import java.nio.file.Path;
-import java.util.Date;
-import java.util.UUID;
 import java.util.stream.Stream;
 import lombok.SneakyThrows;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
+import software.amazon.awssdk.services.sts.model.Credentials;
 
-public class TemporaryPathCredentialsServiceTest extends BaseCRUDTestWithMockCredentials {
+public class TemporaryPathCredentialsServiceTest extends SdkAccessControlBaseCRUDTest {
   private static final String ENDPOINT = "/api/2.1/unity-catalog/temporary-path-credentials";
   private static final String ROOT_PATH = "s3://test-bucket0";
   private static final String CREDENTIAL_NAME = "test_credential";
@@ -61,9 +51,7 @@ public class TemporaryPathCredentialsServiceTest extends BaseCRUDTestWithMockCre
   private static final String ADMIN_USER = "admin";
   private static final String AUTHORIZED_USER = "authorized@test.com";
   private static final String DENIED_USER = "denied@test.com";
-  protected SecurityConfiguration securityConfiguration;
-  protected SecurityContext securityContext;
-  private WebClient client;
+  @Mock AwsCredentialVendor awsCredentialVendor;
   private ExternalLocationOperations externalLocationOperations;
   private CredentialOperations credentialOperations;
 
@@ -126,24 +114,54 @@ public class TemporaryPathCredentialsServiceTest extends BaseCRUDTestWithMockCre
         .build();
   }
 
-  @BeforeEach
+  @Override
+  protected void setUpProperties() {
+    super.setUpProperties();
+    serverProperties.put("s3.bucketPath.0", "s3://test-bucket0");
+    serverProperties.put("s3.accessKey.0", "accessKey0");
+    serverProperties.put("s3.secretKey.0", "secretKey0");
+    serverProperties.put("s3.sessionToken.0", "sessionToken0");
+  }
+
+  @Override
+  protected void setUpCredentialOperations() {
+    setupAwsCredentials();
+
+    cloudCredentialVendor =
+        new CloudCredentialVendor(
+            awsCredentialVendor, null, null, new ServerProperties(serverProperties));
+  }
+
+  private void setupAwsCredentials() {
+    awsCredentialVendor = mock(AwsCredentialVendor.class);
+    Credentials awsCredential =
+        Credentials.builder()
+            .accessKeyId("test-access-key-id")
+            .secretAccessKey("test-secret-access-key")
+            .sessionToken("test-session-token")
+            .build();
+    serverProperties.entrySet().stream()
+        .filter(e -> e.getKey().toString().startsWith("s3.bucketPath"))
+        .map(e -> e.getValue().toString())
+        .forEach(
+            path ->
+                doReturn(awsCredential)
+                    .when(awsCredentialVendor)
+                    .vendAwsCredentials(argThat(isCredentialContextForCloudPath("s3", path))));
+  }
+
+  private ArgumentMatcher<CredentialContext> isCredentialContextForCloudPath(
+      String scheme, String path) {
+    return arg -> arg.getStorageScheme().equals(scheme) && arg.getStorageBase().contains(path);
+  }
+
   @SneakyThrows
+  @BeforeEach
   public void setUp() {
-    // Set admin token to initialize the test environment
     super.setUp();
-    Path configurationFolder = Path.of("etc", "conf");
 
-    securityConfiguration = new SecurityConfiguration(configurationFolder);
-    securityContext =
-        new SecurityContext(configurationFolder, securityConfiguration, "server", INTERNAL);
-
-    switchUser(ADMIN_USER);
-
-    credentialOperations = createCredentialOperations(serverConfig);
-    externalLocationOperations = createExternalLocationOperations(serverConfig);
-
-    getOrCreateUser(AUTHORIZED_USER);
-    getOrCreateUser(DENIED_USER);
+    credentialOperations = createCredentialOperations(adminConfig);
+    externalLocationOperations = createExternalLocationOperations(adminConfig);
 
     credentialOperations.createCredential(
         new CreateCredentialRequest()
@@ -156,42 +174,10 @@ public class TemporaryPathCredentialsServiceTest extends BaseCRUDTestWithMockCre
             .url(EXTERNAL_LOCATION_URL)
             .credentialName(CREDENTIAL_NAME));
 
-    grantPermissionViaApi(
-        AUTHORIZED_USER,
-        EXTERNAL_LOCATION_NAME,
-        EXTERNAL_LOCATION,
-        Privileges.EXTERNAL_USE_LOCATION);
-  }
-
-  @Override
-  protected CatalogOperations createCatalogOperations(ServerConfig serverConfig) {
-    return null;
-  }
-
-  @AfterEach
-  public void cleanUp() {
-    switchUser(ADMIN_USER);
-    deleteUser(AUTHORIZED_USER);
-    deleteUser(DENIED_USER);
-    deleteCredentials(CREDENTIAL_NAME);
-    deleteExternalLocation(EXTERNAL_LOCATION_NAME);
-
-    System.clearProperty("server.authorization");
-
-    SessionFactory sessionFactory = hibernateConfigurator.getSessionFactory();
-    Session session = sessionFactory.openSession();
-    Transaction tx = session.beginTransaction();
-    session.createNativeMutationQuery("delete from casbin_rule").executeUpdate();
-    tx.commit();
-    session.close();
-
-    super.tearDown();
-  }
-
-  @Override
-  protected void setUpProperties() {
-    super.setUpProperties();
-    serverProperties.setProperty("server.authorization", "enable");
+    createTestUser(AUTHORIZED_USER, AUTHORIZED_USER.split("@")[0]);
+    createTestUser(DENIED_USER, DENIED_USER.split("@")[0]);
+    grantPermissions(
+        AUTHORIZED_USER, EXTERNAL_LOCATION, EXTERNAL_LOCATION_NAME, EXTERNAL_USE_LOCATION);
   }
 
   protected CredentialOperations createCredentialOperations(ServerConfig serverConfig) {
@@ -204,115 +190,21 @@ public class TemporaryPathCredentialsServiceTest extends BaseCRUDTestWithMockCre
 
   @ParameterizedTest
   @MethodSource("createPermissionsTestCases")
-  public void testGenerateTemporaryPathCredential(
+  public void testGenerateTemporaryPathCredential2(
       String url, String userEmail, HttpStatus expectedStatus, PathOperation operation) {
     // Arrange
     String requestBody =
         String.format("{\"url\":\"%s\",\"operation\":\"%s\"}", url, operation.toString());
-    switchUser(userEmail);
 
-    // Act
+    String uri = serverConfig.getServerUrl();
+    WebClient webClient =
+        WebClient.builder(uri).auth(AuthToken.ofOAuth2(createTestJwtToken(userEmail))).build();
+
+    //    // Act
     AggregatedHttpResponse response =
-        client.execute(createHeaders(), requestBody).aggregate().join();
+        webClient.execute(createHeaders(), requestBody).aggregate().join();
 
     // Assert
     assertThat(response.status()).isEqualTo(expectedStatus);
-  }
-
-  @SneakyThrows
-  private String createUserIfNotExists(String email) {
-    getOrCreateUser(email);
-
-    return createToken(email);
-  }
-
-  private void grantPermissionViaApi(
-      String principal, String fullName, SecurableType resourceType, Privileges addedPrivilege) {
-    String requestBody =
-        String.format(
-            "{\"changes\":[{\"principal\":\"%s\",\"add\":[\"%s\"]}]}",
-            principal, addedPrivilege.getValue());
-    RequestHeaders headers =
-        RequestHeaders.builder()
-            .method(HttpMethod.PATCH)
-            .path(
-                String.format(
-                    "/api/2.1/unity-catalog/permissions/%s/%s", resourceType.getValue(), fullName))
-            .contentType(MediaType.JSON)
-            .build();
-
-    AggregatedHttpResponse response = client.execute(headers, requestBody).aggregate().join();
-    if (response.status() != HttpStatus.OK) {
-      throw new IllegalStateException("Error setting up permission: " + response.contentUtf8());
-    }
-  }
-
-  private void deleteExternalLocation(String externalLocationName) {
-    try {
-      externalLocationOperations.deleteExternalLocation(externalLocationName);
-    } catch (ApiException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private void deleteCredentials(String credentialName) {
-    try {
-      credentialOperations.deleteCredential(credentialName);
-    } catch (ApiException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private void deleteUser(String email) {
-    Repositories repositories =
-        new Repositories(
-            hibernateConfigurator.getSessionFactory(),
-            new io.unitycatalog.server.utils.ServerProperties(serverProperties));
-
-    try {
-      User user = repositories.getUserRepository().getUserByEmail(email);
-      repositories.getUserRepository().deleteUser(user.getId());
-    } catch (BaseException e) {
-      // User does not exist, nothing to delete
-    }
-  }
-
-  private void getOrCreateUser(String email) {
-    Repositories repositories =
-        new Repositories(
-            hibernateConfigurator.getSessionFactory(),
-            new io.unitycatalog.server.utils.ServerProperties(serverProperties));
-
-    try {
-      repositories.getUserRepository().getUserByEmail(email);
-    } catch (BaseException e) {
-      CreateUser createUser =
-          CreateUser.builder().active(true).email(email).name(email.split("@")[0]).build();
-
-      repositories.getUserRepository().createUser(createUser);
-    }
-  }
-
-  @SneakyThrows
-  private String createToken(String email) {
-    if (email.equals(ADMIN_USER)) {
-      return securityContext.getServiceToken();
-    }
-    return JWT.create()
-        .withSubject(securityContext.getServiceName())
-        .withIssuer(securityContext.getLocalIssuer())
-        .withIssuedAt(new Date())
-        .withKeyId(securityConfiguration.getKeyId())
-        .withJWTId(UUID.randomUUID().toString())
-        .withClaim(JwtClaim.TOKEN_TYPE.key(), JwtTokenType.ACCESS.name())
-        .withClaim(JwtClaim.SUBJECT.key(), email)
-        .sign(securityConfiguration.algorithmRSA());
-  }
-
-  private void switchUser(String email) {
-    String uri = serverConfig.getServerUrl();
-    String token = createUserIfNotExists(email);
-    serverConfig.setAuthToken(token);
-    client = WebClient.builder(uri).auth(AuthToken.ofOAuth2(token)).build();
   }
 }
