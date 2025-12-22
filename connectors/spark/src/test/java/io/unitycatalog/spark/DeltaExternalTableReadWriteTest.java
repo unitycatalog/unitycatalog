295c1ac686f3d6125f5e2487d3a83cbc48253d3a
Alexander Bij <AlexanderBij@godatadriven.com>
2025-12-23 00:24:19 +0100

    Move test, because it's using delta.
    Signed-off-by: Alexander Bij <AlexanderBij@godatadriven.com>

--- connectors/spark/src/test/java/io/unitycatalog/spark/DeltaExternalTableReadWriteTest.java
+++ connectors/spark/src/test/java/io/unitycatalog/spark/DeltaExternalTableReadWriteTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.spark.sql.types.DataTypes;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -33,6 +34,24 @@ public class DeltaExternalTableReadWriteTest extends ExternalTableReadWriteTest 
         testTableReadWrite(tableName);
       }
     }
+  }
+
+  @Test
+  public void testExternalDeltaTableWithDateType() throws IOException {
+    session = createSparkSessionWithCatalogs(SPARK_CATALOG);
+
+    TableSetupOptions options = new TableSetupOptions()
+            .setCatalogName(SPARK_CATALOG)
+            .setTableName(TEST_TABLE);
+
+    sql(String.format(
+            "CREATE TABLE %s (start_date DATE) USING delta LOCATION '%s'",
+            options.fullTableName(), getLocation(options)));
+    sql(String.format("INSERT INTO %s VALUES ('2025-12-22')", options.fullTableName()));
+
+    validateTableSchema(
+            session.table(options.fullTableName()).schema(),
+            Pair.of("start_date", DataTypes.DateType));
   }
 
   @Override
