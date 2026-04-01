aba9c66abc9d826f0e55be68d125f37f1f7b4722
Jake Bellacera <hi@jakebellacera.com>
2026-04-01 11:13:07 -0700

    Update remaining packages
    - Remove eslint-plugin-import as it has an unmet peer dependency with eslint@10
    - Directing prettier to ignore formatting stylesheets in src/content/assets
    - Adding separate lint:fix and format:fix commants to avoid unintentionally writing changes (useful for future CI workflows)
    
    Signed-off-by: Jake Bellacera <hi@jakebellacera.com>

--- docs/eslint.config.mjs
+++ docs/eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import importPlugin from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 
@@ -18,16 +17,6 @@ export default [
   },
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      "import/export": "error",
-      "import/no-duplicates": "warn",
-      "import/order": "error",
-    },
-  },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
   ...tseslint.configs.recommended,
