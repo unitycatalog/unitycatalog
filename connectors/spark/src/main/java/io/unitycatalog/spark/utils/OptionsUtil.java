ae4bcf6bf588546593e19e501ca2d68759224991
Timothy Wang <timothy.art@gmail.com>
2026-04-21 16:36:17 +0000

    Add DeltaRestClientProvider and deltaRestApi.enabled runtime flag
    Signed-off-by: Timothy Wang <timothy.art@gmail.com>

--- connectors/spark/src/main/java/io/unitycatalog/spark/utils/OptionsUtil.java
+++ connectors/spark/src/main/java/io/unitycatalog/spark/utils/OptionsUtil.java
@@ -17,6 +17,9 @@ public class OptionsUtil {
   public static final String SERVER_SIDE_PLANNING_ENABLED = "serverSidePlanning.enabled";
   public static final boolean DEFAULT_SERVER_SIDE_PLANNING_ENABLED = false;
 
+  public static final String DELTA_REST_API_ENABLED = "deltaRestApi.enabled";
+  public static final boolean DEFAULT_DELTA_REST_API_ENABLED = false;
+
   public static boolean getBoolean(
       Map<String, String> props, String property, boolean defaultValue) {
     String value = props.get(property);
