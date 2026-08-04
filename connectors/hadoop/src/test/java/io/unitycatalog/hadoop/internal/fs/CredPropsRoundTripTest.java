8ddbafbc4bcc302d327f9fd61dc27f48f82c2e05
foss-contributor <282724952+foss-contributor@users.noreply.github.com>
2026-08-04 17:37:46 +0000

    add test
    Signed-off-by: foss-contributor <282724952+foss-contributor@users.noreply.github.com>

--- connectors/hadoop/src/test/java/io/unitycatalog/hadoop/internal/fs/CredPropsRoundTripTest.java
+++ connectors/hadoop/src/test/java/io/unitycatalog/hadoop/internal/fs/CredPropsRoundTripTest.java
@@ -67,4 +67,62 @@ class CredPropsRoundTripTest {
     assertThat(resolved.secretAccessKey()).isEqualTo("secret-key");
     assertThat(resolved.sessionToken()).isEqualTo("session-token");
   }
+
+  @Test
+  void multipleCredentialsRoundTripThroughCredScopedFileSystemAndProvider() throws Exception {
+    AwsCredential credential =
+        new AwsCredential("access-key", "secret-key", "session-token", null, LOCATION);
+    CredPropsUtil.genericCredFetcherFactory = (apiClient, credId) -> () -> List.of(credential);
+
+    Configuration initialConf = new Configuration(false);
+    initialConf.setBoolean(UCHadoopConfConstants.UC_CREDENTIAL_CACHE_ENABLED_KEY, false);
+    initialConf.set("fs.s3.impl", RawLocalFileSystem.class.getName());
+    Map<String, String> props =
+        CredPropsUtil.createTableCredProps(
+            /* renewCredEnabled= */ true,
+            /* credScopedFsEnabled= */ true,
+            initialConf,
+            "s3",
+            /* apiClient= */ null,
+            "http://uc",
+            TokenProvider.create(Map.of("type", "static", "token", "token")),
+            "table-id",
+            UCCredentialHadoopConfs.TableOperation.READ,
+            Map.of());
+
+    Configuration confWithCreds = new Configuration(false);
+    props.forEach(confWithCreds::set);
+
+    confWithCreds.setInt(UCHadoopConfConstants.UC_SCOPED_CRED_COUNT_KEY, 2);
+
+    // TODO: Replace these manually encoded properties with CredPropsUtil when it supports
+    // producing properties for multiple vended credentials.
+    setScopedAwsCredential(confWithCreds, 0, "s3://bucket", "parent-ak", "parent-sk", "parent-st");
+    setScopedAwsCredential(confWithCreds, 1, LOCATION, "child-ak", "child-sk", "child-st");
+
+    CredScopedFileSystem fs = new CredScopedFileSystem();
+    fs.initialize(new URI(LOCATION + "/part-0.parquet"), confWithCreds);
+
+    AwsVendedTokenProvider provider = new AwsVendedTokenProvider(fs.getDelegate().getConf());
+    AwsSessionCredentials resolved = (AwsSessionCredentials) provider.resolveCredentials();
+
+    assertThat(provider.accessCredentials().prefix()).isEqualTo(LOCATION);
+    assertThat(resolved.accessKeyId()).isEqualTo("child-ak");
+    assertThat(resolved.secretAccessKey()).isEqualTo("child-sk");
+    assertThat(resolved.sessionToken()).isEqualTo("child-st");
+  }
+
+  private static void setScopedAwsCredential(
+      Configuration conf,
+      int index,
+      String location,
+      String accessKey,
+      String secretKey,
+      String sessionToken) {
+    String namespace = UCHadoopConfConstants.UC_SCOPED_CRED_PREFIX + index + ".";
+    conf.set(namespace + UCHadoopConfConstants.UC_CREDENTIAL_PREFIX_KEY, location);
+    conf.set(namespace + UCHadoopConfConstants.S3A_INIT_ACCESS_KEY, accessKey);
+    conf.set(namespace + UCHadoopConfConstants.S3A_INIT_SECRET_KEY, secretKey);
+    conf.set(namespace + UCHadoopConfConstants.S3A_INIT_SESSION_TOKEN, sessionToken);
+  }
 }
