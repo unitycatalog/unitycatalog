a59188b9961af2ce35920aa0c8ec62551b15c014
Timothy Wang <timothy.art@gmail.com>
2026-09-17 22:39:48 +0000

    [Server] Reuse resource identity for cleanup tasks
    Signed-off-by: Timothy Wang <timothy.art@gmail.com>

--- server/src/main/java/io/unitycatalog/server/persist/dao/StorageCleanupTaskDAO.java
+++ server/src/main/java/io/unitycatalog/server/persist/dao/StorageCleanupTaskDAO.java
@@ -1,20 +1,22 @@
 package io.unitycatalog.server.persist.dao;
 
+import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import java.util.Date;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+/** A pending cleanup task whose ID and name are copied from the deleted resource. */
 @Entity
 @Table(
     name = "uc_storage_cleanup_tasks",
@@ -22,12 +24,16 @@ import lombok.Setter;
       @Index(name = "uc_storage_cleanup_tasks_deleted_idx", columnList = "deleted_at"),
       @Index(name = "uc_storage_cleanup_tasks_lease_idx", columnList = "lease_expires_at")
     })
+@AttributeOverride(
+    name = "id",
+    column = @Column(name = "resource_id", updatable = false, nullable = false))
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class StorageCleanupTaskDAO {
+@EqualsAndHashCode(callSuper = true)
+public class StorageCleanupTaskDAO extends IdentifiableDAO {
   public enum ResourceType {
     TABLE,
     VOLUME,
@@ -39,10 +45,6 @@ public class StorageCleanupTaskDAO {
   @Enumerated(EnumType.STRING)
   @Column(name = "resource_type", nullable = false)
   private ResourceType resourceType;
-
-  @Id
-  @Column(name = "resource_id", updatable = false, nullable = false)
-  private UUID resourceId;
 
   @Column(name = "storage_location", length = 4096, nullable = false)
   private String storageLocation;
